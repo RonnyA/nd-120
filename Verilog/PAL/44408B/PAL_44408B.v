8421d96dd66bd33dda4daffd8bfb73f180df5771
Ronny Hansen <ronnyha@live.no>
2023-12-12 21:48:29 +0100

    Output was VEX , not WEX_n. Fixed

--- Verilog/PAL/44408B/PAL_44408B.v
+++ Verilog/PAL/44408B/PAL_44408B.v
@@ -7,7 +7,7 @@ Replaces 44608 that has a pin RT_n (Assume this signal is now comming from somew
 
 module PAL_44408B(
     input CLK, C4, C3, C2, C1, C0, M1, M0, LCS_n, IDB2, OE_n,
-    output reg RWCS_n, OPCLCS_n, VEX_n, LDEXM_n
+    output reg RWCS_n, OPCLCS_n, VEX, LDEXM_n
 );
 
 // Inverted input signals
@@ -40,13 +40,13 @@ always @(*) begin
         // High-impedance state when OE_n is high (active-low)
         RWCS_n = 1'bz;
         OPCLCS_n = 1'bz;
-        VEX_n = 1'bz;
+        VEX = 1'bz;
         LDEXM_n = 1'bz;
     end else begin
         // Drive outputs normally when OE_n is low
         RWCS_n = ~RWCS_int;
         OPCLCS_n = OPCLCS_n_int;
-        VEX_n = VEX_n_int;
+        VEX = ~VEX_n_int;
         LDEXM_n = ~LDEXM_int;
     end
 end
