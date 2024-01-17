4fce1054a9b7d4dde1cd0e94acf9ac519013d07a
Ronny Hansen <ronnyha@live.no>
2024-01-17 10:40:44 +0100

    Rewrote the logic for 74393

--- Verilog/Shared/support/TTL_74393.v
+++ Verilog/Shared/support/TTL_74393.v
@@ -1,138 +1,32 @@
-/******************************************************************************
- ** Logisim-evolution goes FPGA automatic generated Verilog code             **
- ** https://github.com/logisim-evolution/                                    **
- **                                                                          **
- ** Component : TTL_74393                                                    **
- **                                                                          **
- *****************************************************************************/
+// 74393
+// Dual 4-Stage Binary Ripple Counter
+// Implementing just one of the clocks. Instantiate two modules to get both
+// Documentation: https://www.onsemi.com/pdf/datasheet/mc74hc393a-d.pdf
 
-module TTL_74393( A_n,
-                  CLR,
-                  QA,
-                  QB,
-                  QC,
-                  QD );
+module TTL_74393 (
+    input CLK_n,     // Clock input (Count on negative edge)
+    input RESET,     // Asynchronous clear (active high) 
 
-   /*******************************************************************************
-   ** The inputs are defined here                                                **
-   *******************************************************************************/
-   input A_n;
-   input CLR;
+    output QA,
+    output QB,
+    output QC,
+    output QD
+);
 
-   /*******************************************************************************
-   ** The outputs are defined here                                               **
-   *******************************************************************************/
-   output QA;
-   output QB;
-   output QC;
-   output QD;
+reg [3:0] counter1 = 4'b0000;
 
-   /*******************************************************************************
-   ** The wires are defined here                                                 **
-   *******************************************************************************/
-   wire s_logisimNet0;
-   wire s_logisimNet1;
-   wire s_logisimNet10;
-   wire s_logisimNet11;
-   wire s_logisimNet12;
-   wire s_logisimNet13;
-   wire s_logisimNet14;
-   wire s_logisimNet15;
-   wire s_logisimNet16;
-   wire s_logisimNet17;
-   wire s_logisimNet2;
-   wire s_logisimNet3;
-   wire s_logisimNet4;
-   wire s_logisimNet5;
-   wire s_logisimNet6;
-   wire s_logisimNet7;
-   wire s_logisimNet8;
-   wire s_logisimNet9;
 
-   /*******************************************************************************
-   ** The module functionality is described here                                 **
-   *******************************************************************************/
+// First 4-bit counter
+always @(negedge CLK_n or posedge RESET) begin
+    if (RESET)
+        counter1 <= 4'b0000;
+    else
+        counter1 <= counter1 + 1;
+end
 
-   /*******************************************************************************
-   ** Here all input connections are defined                                     **
-   *******************************************************************************/
-   assign s_logisimNet1  = CLR;
-   assign s_logisimNet12 = A_n;
-
-   /*******************************************************************************
-   ** Here all output connections are defined                                    **
-   *******************************************************************************/
-   assign QA = s_logisimNet15;
-   assign QB = s_logisimNet11;
-   assign QC = s_logisimNet16;
-   assign QD = s_logisimNet17;
-
-   /*******************************************************************************
-   ** Here all in-lined components are defined                                   **
-   *******************************************************************************/
-
-   // NOT Gate
-   assign s_logisimNet15 = ~s_logisimNet9;
-
-   // NOT Gate
-   assign s_logisimNet11 = ~s_logisimNet14;
-
-   // NOT Gate
-   assign s_logisimNet16 = ~s_logisimNet10;
-
-   // NOT Gate
-   assign s_logisimNet17 = ~s_logisimNet8;
-
-   // NOT Gate
-   assign s_logisimNet4 = ~s_logisimNet7;
-
-   // NOT Gate
-   assign s_logisimNet13 = ~s_logisimNet2;
-
-   // NOT Gate
-   assign s_logisimNet5 = ~s_logisimNet3;
-
-   // NOT Gate
-   assign s_logisimNet6 = ~s_logisimNet0;
-
-   /*******************************************************************************
-   ** Here all normal components are defined                                     **
-   *******************************************************************************/
-   D_FLIPFLOP #(.invertClockEnable(1))
-      MEMORY_1 (.clock(s_logisimNet12),
-                .d(s_logisimNet4),
-                .preset(1'b0),
-                .q(s_logisimNet7),
-                .qBar(s_logisimNet9),
-                .reset(s_logisimNet1),
-                .tick(1'b1));
-
-   D_FLIPFLOP #(.invertClockEnable(0))
-      MEMORY_2 (.clock(s_logisimNet7),
-                .d(s_logisimNet13),
-                .preset(1'b0),
-                .q(s_logisimNet2),
-                .qBar(s_logisimNet14),
-                .reset(s_logisimNet1),
-                .tick(1'b1));
-
-   D_FLIPFLOP #(.invertClockEnable(0))
-      MEMORY_3 (.clock(s_logisimNet2),
-                .d(s_logisimNet5),
-                .preset(1'b0),
-                .q(s_logisimNet3),
-                .qBar(s_logisimNet10),
-                .reset(s_logisimNet1),
-                .tick(1'b1));
-
-   D_FLIPFLOP #(.invertClockEnable(0))
-      MEMORY_4 (.clock(s_logisimNet3),
-                .d(s_logisimNet6),
-                .preset(1'b0),
-                .q(s_logisimNet0),
-                .qBar(s_logisimNet8),
-                .reset(s_logisimNet1),
-                .tick(1'b1));
-
+assign QA = counter1[0];
+assign QB = counter1[1];
+assign QC = counter1[2];
+assign QD = counter1[3];
 
 endmodule
