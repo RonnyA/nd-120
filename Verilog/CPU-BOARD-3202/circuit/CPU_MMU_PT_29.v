5235500b8f3b379bb30199cad70052ce583bbb88
Ronny Hansen <ronnyha@live.no>
2024-04-20 11:49:49 +0200

    Refactored _io signal to _IN and _OUT

--- Verilog/CPU-BOARD-3202/circuit/CPU_MMU_PT_29.v
+++ Verilog/CPU-BOARD-3202/circuit/CPU_MMU_PT_29.v
@@ -4,21 +4,26 @@
 ** CACHE                                                                 **
 ** SHEET 29 of 50                                                        **
 **                                                                       ** 
-** Last reviewed: 12-FEB-2024                                            **
+** Last reviewed: 20-APRIL-2024                                          **
 ** Ronny Hansen                                                          **
 ***************************************************************************/
 
 module CPU_MMU_PT_29(
-   input [10:0] LA_20_10,
+   input sysclk,      // System clock in FPGA
+   input sys_rst_n,   // System reset in FPGA
 
-   input        EPMAP_n,
-   input        EPT_n,   
-   input        WCLIM_n,
-   input        WMAP_n,
+   input [10:0] LA_20_10,  // 11 bit addressing into PT chips
+
+   input        EPMAP_n,   // Enable EPMAP chips (Extended map?)
+   input        EPT_n,     // Enable PT chips (Chip select for PT chips). Combine with WMAP_n to write to PT chips (Chip 24G and Chip 25G)
+   input        WCLIM_n,   // Write to RAM chip with 1 bit Data being PPN hi bit (bit ppn 25)
+   input        WMAP_n,    // Write MAPPING signal
    
+   input  [15:0] PPN_25_10_IN, // Bidirectional
+   output [15:0] PPN_25_10_OUT, // Bidirectional
 
-   inout [15:0] PPN_25_10, // Bidirectional
-   inout [15:0] PT_15_0,   // Bidirectional
+   input  [15:0] PT_15_0_IN,   // Bidirectional
+   output [15:0] PT_15_0_OUT,   // Bidirectional
 
    output        WCINH_n                      
    );
@@ -26,20 +31,16 @@ module CPU_MMU_PT_29(
    /*******************************************************************************
    ** The wires are defined here                                                 **
    *******************************************************************************/
-   wire [10:0] s_logisimBus0;
-   wire [15:0] s_logisimBus4;
-   wire [15:0] s_logisimBus7;
-   wire        s_logisimNet1;
-   wire        s_logisimNet10;
-   wire        s_logisimNet12;
-   wire        s_logisimNet14;
-   wire        s_logisimNet15;
-   wire        s_logisimNet17;
-   wire        s_logisimNet18;
-   wire        s_logisimNet2;
-   wire        s_logisimNet3;
-   wire        s_logisimNet5;
-   wire        s_logisimNet8;
+   wire [10:0] s_la_20_10;
+   wire [15:0] s_pt_15_0_in;
+   wire [15:0] s_pt_15_0_out;
+   wire [15:0] s_ppn_25_10_in;
+   wire [15:0] s_ppn_25_10_out;
+   wire        s_wclim_n;
+   wire        s_wcinh_n;
+   wire        s_ept_n;
+   wire        s_epmap_n;
+   wire        s_wmap_n;
 
    /*******************************************************************************
    ** The module functionality is described here                                 **
@@ -48,75 +49,103 @@ module CPU_MMU_PT_29(
    /*******************************************************************************
    ** Here all input connections are defined                                     **
    *******************************************************************************/
-   assign s_logisimBus0[10:0] = LA_20_10;
-   assign s_logisimNet10      = WCLIM_n;
-   assign s_logisimNet18      = EPT_n;
-   assign s_logisimNet3       = EPMAP_n;
-   assign s_logisimNet8       = WMAP_n;
+   assign s_la_20_10[10:0] = LA_20_10;
+   assign s_wclim_n        = WCLIM_n;
+   assign s_ept_n          = EPT_n;
+   assign s_epmap_n        = EPMAP_n;
+   assign s_wmap_n         = WMAP_n;
+
+   assign s_ppn_25_10_in = PPN_25_10_IN;
+   assign s_pt_15_0_in   = PT_15_0_IN;
 
    /*******************************************************************************
    ** Here all output connections are defined                                    **
    *******************************************************************************/
-   assign PPN_25_10    = s_logisimBus7[15:0];
-   assign PT_15_0      = s_logisimBus4[15:0];
-   assign WCINH_n      = s_logisimNet15;
+   assign PPN_25_10_OUT    = s_ppn_25_10_out[15:0];
+   assign PT_15_0_OUT      = s_pt_15_0_out[15:0];
+   assign WCINH_n          = s_wcinh_n;
 
    /*******************************************************************************
    ** Here all in-lined components are defined                                   **
    *******************************************************************************/
 
    // Ground
-   assign  s_logisimNet12  =  1'b0;
-
-
-   // Ground
-   assign  s_logisimNet1  =  1'b0;
-
-
-   // Ground
-   assign  s_logisimNet14  =  1'b0;
-
-
-   // Ground
-   assign  s_logisimNet2  =  1'b0;
-
-
-   // Ground
-   assign  s_logisimNet17  =  1'b0;
-
+   assign  s_gnd  =  1'b0;
 
    /*******************************************************************************
    ** Here all sub-circuits are defined                                          **
    *******************************************************************************/
 
-   TMM2018D_25   CHIP_24G (.A0_A10(s_logisimBus0[10:0]),
-                           .CS_n(s_logisimNet18),
-                           .D0_D7(s_logisimBus4[15:8]),
-                           .OE_n(s_logisimNet12),
-                           .W_n(s_logisimNet8));
+        
+      /**** Page Table - PT_25_10 ****/
 
-   TMM2018D_25   CHIP_25G (.A0_A10(s_logisimBus0[10:0]),
-                           .CS_n(s_logisimNet18),
-                           .D0_D7(s_logisimBus4[7:0]),
-                           .OE_n(s_logisimNet1),
-                           .W_n(s_logisimNet8));
+      // PT_25_18 (hi part of PPN_25_10)
+      TMM2018D_25   CHIP_24G (
+                              .clk(sysclk),
+                              .reset_n(sys_rst_n),
 
-   IMS1403_25   CHIP_20G (.A0_A13(s_logisimBus7[13:0]),
-                          .CE_n(s_logisimNet14),
-                          .D(s_logisimBus7[15]),
-                          .Q(s_logisimNet15),
-                          .W_n(s_logisimNet10));
+                              .ADDRESS(s_la_20_10[10:0]),
+                              .CS_n(s_ept_n),
+                              .D(s_pt_15_0_in[15:8]),
+                              .D_OUT(s_pt_15_0_out[15:8]),
+                              .OE_n(s_gnd),
+                              .W_n(s_wmap_n));
 
-   TMM2018D_25   CHIP_22G (.A0_A10(s_logisimBus0[10:0]),
-                           .CS_n(s_logisimNet3),
-                           .D0_D7(s_logisimBus7[15:8]),
-                           .OE_n(s_logisimNet17),
-                           .W_n(s_logisimNet8));
 
-   TMM2018D_25   CHIP_23G (.A0_A10(s_logisimBus0[10:0]),
-                           .CS_n(s_logisimNet3),
-                           .D0_D7(s_logisimBus7[7:0]),
-                           .OE_n(s_logisimNet2),
-                           .W_n(s_logisimNet8));
+      // PT_17_10 (lo part of PT_25_10)
+      TMM2018D_25   CHIP_25G (
+                              .clk(sysclk),
+                              .reset_n(sys_rst_n),
+
+                              .ADDRESS(s_la_20_10[10:0]),
+                              .CS_n(s_ept_n),
+                              .D(s_pt_15_0_in[7:0]),
+                              .D_OUT(s_pt_15_0_out[7:0]),
+                              .OE_n(s_gnd),
+                              .W_n(s_wmap_n));
+
+      /**** PPN tabler - PPN_25_10 ****/
+
+      // PPN_25_18 (hi part of PPN_25_10)
+      TMM2018D_25   CHIP_22G (
+                              .clk(sysclk),
+                              .reset_n(sys_rst_n),
+
+                              .ADDRESS(s_la_20_10[10:0]),
+                              .CS_n(s_epmap_n),
+                              .D(s_ppn_25_10_in[15:8]),
+                              .D_OUT(s_ppn_25_10_out[15:8]),
+                              .OE_n(s_gnd),
+                              .W_n(s_wmap_n));
+
+
+      // PPN_17_10 (lo part of PPN_25_10)
+      TMM2018D_25   CHIP_23G (
+                              .clk(sysclk),
+                              .reset_n(sys_rst_n),
+                        
+                              .ADDRESS(s_la_20_10[10:0]),
+                              .CS_n(s_epmap_n),
+                              .D(s_ppn_25_10_in[7:0]),
+                              .D_OUT(s_ppn_25_10_out[7:0]),
+                              .OE_n(s_gnd),
+                              .W_n(s_wmap_n));
+
+
+
+
+
+  
+      // PPN adressing into RAM chip with 1 bit Data being PPN hi bit (bit ppn 25). Write to RAM when WCLIM_n is low
+      IMS1403_25   CHIP_20G (
+                              .clk(sysclk),
+                              .reset_n(sys_rst_n),
+                              .ADDRESS(s_ppn_25_10[13:0]), // Addres bit s_ppn_25_10[14] not used.
+                              .CE_n(s_gnd),
+                              .D(s_ppn_25_10[15]),
+                              .Q(s_wcinh_n),
+                              .W_n(s_wclim_n)
+                              );
+
 
 endmodule
