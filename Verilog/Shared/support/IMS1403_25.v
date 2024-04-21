4978678c37f982f3c51656f7b160a85860245738
Ronny Hansen <ronnyha@live.no>
2024-04-21 15:22:57 +0200

    Refactored to use BLOCK RAM

--- Verilog/Shared/support/IMS1403_25.v
+++ Verilog/Shared/support/IMS1403_25.v
@@ -4,26 +4,59 @@
  *****************************************************************************/
 
 module IMS1403_25( 
-   input [13:0] A0_A13,
+   input wire    clk, // Clock input (needed for BLOCK RAM)
+   input wire    reset_n, // FPGA Reset input (active low)
+
+   input [13:0] ADDRESS,   // 14 bits address
    input        CE_n,
    input        D,
    input        W_n,
-   output Q
+   output       Q
 );
+
+
+   integer i;
+
+   /*******************************************************************************
+   ** Signals                                                                    **
+   *******************************************************************************/
+   reg  data_out;    // Output data register
+   wire data_in;     // Input data from the bus
+
+   assign data_in = D; // Connect input data
+
+   assign Q  = (!CE_n && W_n) ? data_out : 1'bz;  //Q is high impedance when W_n (write) is active and when chip is not selected
+
 
 
   // Define the memory array
 
-   /* verilator lint_off LITENDIAN */
-   reg [0:16383] memory;
 
-   /* verilator lint_on LITENDIAN */   
-   /* verilator lint_off LATCH */
-   always @* begin
-      if (!CE_n && !W_n)  // Chip enabled and Write Enabled
-         memory[A0_A13] = D;      
+   /*******************************************************************************
+   ** Memory array using block RAM                                               **
+   *******************************************************************************/
+   (* ram_style = "block" *) reg [16383:0] memory_array;
+   
+     
+   always @(posedge clk) begin
+
+      if (!reset_n) begin
+            /* verilator lint_off BLKANDNBLK */ 
+            /* verilator lint_off BLKSEQ */
+            // Reset operation: set all memory locations to 0                
+            for (i = 0; i < 16383; i = i + 1) begin
+               memory_array[i] = 1'b0; // none delayed initialisation
+            end               
+            /* verilator lint_on BLKSEQ */
+      end
+
+      if (!CE_n) begin
+         if (!W_n) begin
+            memory_array[ADDRESS] <= data_in;  // Write operation
+         end else begin
+            data_out <= memory_array[ADDRESS]; // Read operation
+         end
+      end
    end
-
-   assign Q = CE_n ? 1'bz : W_n ? memory[A0_A13] : 1'bz; // Q is high impedance when W_n (write) is active   
 
 endmodule
