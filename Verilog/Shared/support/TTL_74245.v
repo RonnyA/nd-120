ba661dd37fe20cee42562871199e4c4b1b8505b1
Ronny Hansen <ronnyha@live.no>
2024-04-21 15:21:24 +0200

    refactored inout to IN and OUT

--- Verilog/Shared/support/TTL_74245.v
+++ Verilog/Shared/support/TTL_74245.v
@@ -6,8 +6,12 @@
  *****************************************************************************/
 
 module TTL_74245( 
-    inout [7:0] A,       // A-side 8-bit port
-    inout [7:0] B,       // B-side 8-bit bidirectional port/bus
+    input  [7:0] A,        // A-side 8-bit port
+    output [7:0] A_OUT,    // A-side 8-bit port
+
+    input  [7:0] B,        // B-side 8-bit bidirectional port/bus
+    output [7:0] B_OUT,    // B-side 8-bit bidirectional port/bus
+
     input DIR,           // Direction control
     input OE_n           // Output enable
 );
@@ -24,10 +28,10 @@ always @(*) begin
 end
 
 // Assign the bidirectional bus with respect to OE
-assign B = (OE_n == 0 && DIR == 1) ? internalBus : 8'bz;
+assign B_OUT = (OE_n == 0 && DIR == 1) ? internalBus : 8'bz;
 
 // Output to A when receiving from B with respect to OE (OE_n==1 means "isolated". Don't write to A or B)
-assign A = (OE_n == 0 && DIR == 0) ? internalBus : 8'bz;
+assign A_OUT = (OE_n == 0 && DIR == 0) ? internalBus : 8'bz;
 
 endmodule
 
