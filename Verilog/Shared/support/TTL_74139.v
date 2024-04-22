685bfd67e6fb9071b90e457a03c91f0185e27813
Ronny Hansen <ronnyha@live.no>
2024-04-22 09:06:32 +0200

    Bugfix: Output was'nt correct negated

--- Verilog/Shared/support/TTL_74139.v
+++ Verilog/Shared/support/TTL_74139.v
@@ -25,20 +25,20 @@ module TTL_74139(
     // Check the enable signal and set outputs
     // When G_n is low, one output goes low based on the combination of B and A
     // When G_n is high, all outputs are high
-    assign Y1_0_n = G1_n ? 1'b1 : ((~B1) & (~A1));  // Y[0] is low when B=0, A=0
-    assign Y1_1_n = G1_n ? 1'b1 : ((~B1) & A1);     // Y[1] is low when B=0, A=1
-    assign Y1_2_n = G1_n ? 1'b1 : (B1 & (~A1));     // Y[2] is low when B=1, A=0
-    assign Y1_3_n = G1_n ? 1'b1 : (B1 & A1);        // Y[3] is low when B=1, A=1
+    assign Y1_0_n = G1_n ? 1'b1 : ~((~B1) & (~A1));  // Y[0] is low when B=0, A=0
+    assign Y1_1_n = G1_n ? 1'b1 : ~((~B1) & A1);     // Y[1] is low when B=0, A=1
+    assign Y1_2_n = G1_n ? 1'b1 : ~(B1 & (~A1));     // Y[2] is low when B=1, A=0
+    assign Y1_3_n = G1_n ? 1'b1 : ~(B1 & A1);        // Y[3] is low when B=1, A=1
 
    // DECODER 2   
 
     // Check the enable signal and set outputs
     // When G_n is low, one output goes low based on the combination of B and A
     // When G_n is high, all outputs are high
-    assign Y2_0_n = G2_n ? 1'b1 : ((~B2) & (~A2));  // Y[0] is low when B=0, A=0
-    assign Y2_1_n = G2_n ? 1'b1 : ((~B2) & A2);     // Y[1] is low when B=0, A=1
-    assign Y2_2_n = G2_n ? 1'b1 : (B2 & (~A2));     // Y[2] is low when B=1, A=0
-    assign Y2_3_n = G2_n ? 1'b1 : (B2 & A2);        // Y[3] is low when B=1, A=1
+    assign Y2_0_n = G2_n ? 1'b1 : ~((~B2) & (~A2));  // Y[0] is low when B=0, A=0
+    assign Y2_1_n = G2_n ? 1'b1 : ~((~B2) & A2);     // Y[1] is low when B=0, A=1
+    assign Y2_2_n = G2_n ? 1'b1 : ~(B2 & (~A2));     // Y[2] is low when B=1, A=0
+    assign Y2_3_n = G2_n ? 1'b1 : ~(B2 & A2);        // Y[3] is low when B=1, A=1
 
 
 endmodule
