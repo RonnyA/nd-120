eac02cdbcd1ee53970181f3eda671f7660ad63c5
Ronny Hansen <ronnyha@live.no>
2024-04-23 17:03:34 +0200

    Renamed output signal to Y (which is correct) away from Q (signaling negated, which it is not)

--- Verilog/Shared/support/AM29C821.v
+++ Verilog/Shared/support/AM29C821.v
@@ -9,20 +9,20 @@ module AM29C821 (
     input wire [9:0] D,   // 10 Bit Data inputs
     input wire CK,        // Clock input (Latching on rising edge of CK)
     input wire OE_n,      // Output Enable (active low)
-    output wire [9:0] Q   // Outputs
+    output wire [9:0] Y   // Outputs
 );
 
-    reg [9:0] Q_Latch;  // Internal latch
+    reg [9:0] Y_Latch;  // Internal latch
 
    
     // Latch operation on rising edge of CK
     always @(posedge CK) begin
-        Q_Latch <= D;  // Latch data on rising edge of CK
+        Y_Latch <= D;  // Latch data on rising edge of CK
     end
 
     // Output control
     // When OC_n is low (active), outputs reflect the latched data
     // When OC_n is high, outputs are in high-impedance state
-    assign Q = (OE_n == 1'b0) ? Q_Latch : 10'bz;
+    assign Y = (OE_n == 1'b0) ? Y_Latch : 10'bz;
 
 endmodule
