7290bbce5f3b61d552c994c232971995cea4bd58
Ronny Hansen <ronnyha@live.no>
2024-04-23 16:45:51 +0200

    Adding sysclk to support BLOCK RAM Refactor of signal names IN and OUT instead of inout pins. Removed AND/OR submodules and implemnted logic directly in verilog

--- Verilog/CPU-BOARD-3202/circuit/CYC_36.v
+++ Verilog/CPU-BOARD-3202/circuit/CYC_36.v
@@ -10,6 +10,11 @@
  
 
 module CYC_36( 
+/* verilator lint_off UNUSEDSIGNAL */   
+               input sysclk, // System clock in FPGA
+               input sys_rst_n, // System reset in FPGA
+/* verilator lint_on UNUSEDSIGNAL */              
+
                input OSC,
 
                input ACOND_n,               
@@ -19,8 +24,7 @@ module CYC_36(
                input CSALUI8,
                input CSALUM0,
                input CSALUM1,
-               input CSDELAY0,
-               input CSDELAY1,
+               input [1:0] CSDELAY_1_0,               
                input CSDLY,
                input CSECOND,
                input CSLOOP,
@@ -66,15 +70,14 @@ module CYC_36(
    *******************************************************************************/
    wire [2:0] s_logisimBus33;   
    wire       s_PD4;
-   wire       s_logisimNet10;
+   wire       s_lua12;
    wire       s_logisimNet11;
    wire       s_logisimNet12;
    wire       s_logisimNet13;
    wire       s_logisimNet14;
    // wire       s_DLSHADOW; // not used ??
    wire       s_logisimNet16;
-   wire       s_mr_n;
-   wire       s_logisimNet18;
+   wire       s_mr_n;   
    wire       s_logisimNet19;
    wire       s_logisimNet2;
    wire       s_logisimNet20;   
@@ -83,10 +86,10 @@ module CYC_36(
    wire       s_FORM_n;
    wire       s_logisimNet26;
    wire       s_logisimNet27;   
-   wire       s_logisimNet29;
-   //wire       s_MDLY_n; (not used)
-   wire       s_logisimNet30;
-   wire       s_logisimNet31;
+   wire       s_lcs_n;
+   //wire       s_MDLY_n;//(not used)
+   wire       s_mclk_n;
+   wire       s_vcc;
    wire       s_MAP_n;
    // wire       s_DMA12_n; (not used)
    wire       s_logisimNet36;
@@ -97,7 +100,7 @@ module CYC_36(
    wire       s_PD1;
    //wire       s_logisimNet41;
    wire       s_SLCOND_n;
-   wire       s_logisimNet42;
+   wire       s_lcs;
    wire       s_logisimNet43;
    wire       s_logisimNet44;
    wire       s_logisimNet45;
@@ -106,10 +109,10 @@ module CYC_36(
    wire       s_logisimNet48;
    wire       s_logisimNet49;
    wire       s_TERM_n;
-   wire       s_logisimNet50;   
-   wire       s_logisimNet52;
-   wire       s_logisimNet53;
-   wire       s_logisimNet54;
+   wire       s_dly0_n;   
+   wire       c_csdly;
+   wire       s_logisimNet53;   
+   wire [1:0] s_csdelay_1_0;
    wire       s_logisimNet55;
    wire       s_logisimNet56;
    wire       s_logisimNet57;
@@ -129,50 +132,43 @@ module CYC_36(
    wire       s_logisimNet70;
    wire       s_logisimNet71;
    wire       s_RRF_n;
-   wire       s_logisimNet8;
-   wire       s_logisimNet9;
-
-   /*******************************************************************************
-   ** The module functionality is described here                                 **
-   *******************************************************************************/
+   wire       s_csecond;
+   wire       s_acond_n;
 
    /*******************************************************************************
    ** Here all input connections are defined                                     **
    *******************************************************************************/
-   assign s_PD4  = PD4;
-   assign s_logisimNet10 = LUA12;
+   assign s_PD4          = PD4;
+   assign s_lua12        = LUA12;
    assign s_logisimNet11 = CSALUM1;
    assign s_logisimNet12 = CSALUI8;
    assign s_logisimNet13 = LBA3;
    assign s_logisimNet14 = LBA0;
-   assign s_mr_n         = MR_n;
-   assign s_logisimNet18 = CSDELAY0;
+   assign s_mr_n         = MR_n;   
    assign s_FORM_n       = FORM_n;
    assign s_logisimNet26 = RWCS_n;
    assign s_logisimNet27 = VEX;
    assign s_logisimNet38 = IORQ_n;
    assign s_logisimNet39 = RT_n;
-   assign s_PD1 = PD1;
+   assign s_PD1          = PD1;
    assign s_logisimNet43 = HIT;
    assign s_logisimNet46 = MREQ_n;
-   assign s_logisimNet52 = CSDLY;
-   assign s_logisimNet53 = CSLOOP;
-   assign s_logisimNet54 = CSDELAY1;
+   assign c_csdly        = CSDLY;
+   assign s_logisimNet53 = CSLOOP;   
+   assign s_csdelay_1_0[1:0]  = CSDELAY_1_0[1:0];
    assign s_logisimNet55 = CSALUM0;
    assign s_logisimNet56 = CSALUI7;
    assign s_logisimNet57 = LBA1;
    assign s_logisimNet59 = TRAP_n;
    assign s_logisimNet6  = BRK_n;   
-   assign s_SLOW_n = SLOW_n;
-   //assign s_logisimNet62 = LSHADOW;
-   assign s_LSHADOW = LSHADOW;
+   assign s_SLOW_n       = SLOW_n;   
+   assign s_LSHADOW      = LSHADOW;
    assign s_OSC          = OSC;
    assign s_logisimNet65 = CGNTCACT_n;   
-   assign s_SHORT_n = SHORT_n;
-   //assign s_logisimNet72 = RRF_n;
-   assign s_RRF_n = RRF_n;
-   assign s_logisimNet8  = CSECOND;
-   assign s_logisimNet9  = ACOND_n;
+   assign s_SHORT_n      = SHORT_n;   
+   assign s_RRF_n        = RRF_n;
+   assign s_csecond      = CSECOND;
+   assign s_acond_n      = ACOND_n;
 
    /*******************************************************************************
    ** Here all output connections are defined                                    **
@@ -184,7 +180,7 @@ module CYC_36(
    assign CYD      = s_logisimNet47;
    assign EORF_n   = s_logisimNet70;
    assign ETRAP_n  = s_logisimNet71;
-   assign LCS_n    = s_logisimNet29;
+   assign LCS_n    = s_lcs_n;
    assign MACLK    = s_logisimNet45;
    assign MAP_n    = s_MAP_n;
    assign MCLK     = s_logisimNet66;
@@ -197,11 +193,11 @@ module CYC_36(
    *******************************************************************************/
 
    // Power
-   assign  s_logisimNet31  =  1'b1;
+   assign  s_vcc  =  1'b1;
 
 
    // NOT Gate
-   assign s_logisimNet42 = ~s_logisimNet29;
+   assign s_lcs = ~s_lcs_n;
 
    /*******************************************************************************
    ** Here all normal components are defined                                     **
@@ -215,51 +211,81 @@ module CYC_36(
    
    assign s_logisimNet36 = (s_mr_n & s_logisimNet63);
 
+   /*
    NAND_GATE #(.BubblesMask(2'b00))
-      GATES_2 (.input1(s_logisimNet31),
+      GATES_2 (.input1(s_vcc),
                .input2(s_TERM_n),
                .result(s_CLK));
+   */
+   assign s_CLK = ~(s_vcc & s_TERM_n); // CAN be optimized to an negator
 
+   /*
    NAND_GATE #(.BubblesMask(2'b00))
       GATES_3 (.input1(s_TERM_n),
-               .input2(s_logisimNet30),
+               .input2(s_mclk_n),
                .result(s_logisimNet66));
+   */
+   assign s_logisimNet66 = ~(s_TERM_n & s_mclk_n); 
 
+   /*
    NAND_GATE #(.BubblesMask(2'b00))
       GATES_4 (.input1(s_TERM_n),
                .input2(s_logisimNet20),
                .result(s_logisimNet45));
+   */
+   assign s_logisimNet45 = ~(s_TERM_n & s_logisimNet20);
 
+   /*
    AND_GATE #(.BubblesMask(2'b00))
       GATES_5 (.input1(s_TERM_n),
                .input2(s_logisimNet44),
                .result(s_logisimNet22));
 
+   */
+   assign s_logisimNet22 = (s_TERM_n & s_logisimNet44);
+   
+   /*
    NOR_GATE #(.BubblesMask(2'b00))
       GATES_6 (.input1(s_TERM_n),
-               .input2(s_logisimNet42),
+               .input2(s_lcs),
                .result(s_logisimNet2));
+   */
+   assign s_logisimNet2 = ~(s_TERM_n | s_lcs);
 
+   /*
    OR_GATE #(.BubblesMask(2'b00))
       GATES_7 (.input1(s_LSHADOW),
                .input2(s_logisimNet39),
                .result(s_logisimNet67));
+   */
 
+   assign s_logisimNet67 = (s_LSHADOW | s_logisimNet39);
+
+   /*
    OR_GATE #(.BubblesMask(2'b00))
       GATES_8 (.input1(s_logisimNet46),
                .input2(s_LSHADOW),
                .result(s_logisimNet48));
+   */
+   assign s_logisimNet48 = (s_logisimNet46 | s_LSHADOW);
 
+
+   /*
    NAND_GATE #(.BubblesMask(2'b00))
       GATES_9 (.input1(s_logisimNet48),
                .input2(s_logisimNet38),
                .result(s_logisimNet63));
+   */
+   assign s_logisimNet63 = ~(s_logisimNet48 & s_logisimNet38);
 
+   /*
    NAND_GATE #(.BubblesMask(2'b00))
       GATES_10 (.input1(s_logisimNet38),
                 .input2(s_logisimNet67),
                 .result(s_logisimNet49));
 
+   */
+   assign s_logisimNet49 = ~(s_logisimNet38 & s_logisimNet67);
 
    /*******************************************************************************
    ** Here all sub-circuits are defined                                          **
@@ -271,8 +297,8 @@ module CYC_36(
 
                                  //I0-I7
                                   .DLY1_n(s_logisimNet16),     //I0
-                                  .DLY0_n(s_logisimNet50),     //I1
-                                  .CSDELAY0(s_logisimNet18),   //I2   
+                                  .DLY0_n(s_dly0_n),           //I1
+                                  .CSDELAY0(s_csdelay_1_0[0]), //I2   
                                   .WAIT1(s_logisimNet36),      //I3
                                   .WAIT2(s_logisimNet49),      //I4
                                   .CGNTCACT_n(s_logisimNet65), //I5
@@ -303,7 +329,7 @@ module CYC_36(
                                    .RWCS_n(s_logisimNet26),    // I7
                                    .TRAP_n(s_logisimNet59),    // I8
                                    .VEX(s_logisimNet27),       // I9
-                                   .MCLK_n(s_logisimNet30),    // Y0_n
+                                   .MCLK_n(s_mclk_n),          // Y0_n
                                    .MACLK_n(s_logisimNet20),   // Y1_n
                                    .WRFSTB(s_logisimNet69),    // B0_n
                                    .CYD(s_logisimNet47),       // B1_n
@@ -319,22 +345,23 @@ module CYC_36(
                                  .OE_n(s_PD1),                 //OE_n
 
                                  
-                                 .CSDELAY0(s_logisimNet18),    //I0
-                                 .CSDLY(s_logisimNet52),       //I1
-                                 .CSECOND(s_logisimNet8),      //I2
+                                 .CSDELAY0(s_csdelay_1_0[0]),  //I0
+                                 .CSDLY(c_csdly),              //I1
+                                 .CSECOND(s_csecond),          //I2
                                  .CSLOOP(s_logisimNet53),      //I3
-                                 .ACOND_n(s_logisimNet9),      //I4   
+                                 .ACOND_n(s_acond_n),          //I4   
                                  .MR_n(s_mr_n),                //I5
-                                 .LUA12(s_logisimNet10),       //I6
+                                 .LUA12(s_lua12),              //I6
                                  .MAP_n(s_MAP_n),              //I7
 
                                  //Q0-Q3 (clocked and 3-state)
-                                 .LCS_n(s_logisimNet29),       //Q0_n
-                                 // .MDLY_n(s_MDLY_n),         //Q1_n (not used)
+                                 .LCS_n(s_lcs_n),              //Q0_n
+                                 //.MDLY_n(s_MDLY_n),          //Q1_n  (not used)
                                  //.DMA12_n(s_DMA12_n),        //Q2_n  (not used)
-                                 //.DMAP_n(s_DMAP_n),          //Q3_n   (not used)
+                                 //.DMA12_n(),                 //Q2_n  (not used)
+                                 //.DMAP_n(s_DMAP_n),          //Q3_n  (not used)
 
-                                 .DLY0_n(s_logisimNet50),      //B0_n
+                                 .DLY0_n(s_dly0_n),            //B0_n
                                  .SLCOND_n(s_SLCOND_n)         //B2_n
 
                                  );
@@ -342,7 +369,7 @@ module CYC_36(
    PAL_44404C   PAL_44404_UCYIN1 (.CLK(s_CLK),                 //CK
                                   .OE_n(s_PD1),                //OE_n
                                                                
-                                 .CSDELAY1(s_logisimNet54),    //I0
+                                 .CSDELAY1(s_csdelay_1_0[1]),  //I0
                                  .CSALUM1(s_logisimNet11),     //I1
                                  .CSALUM0(s_logisimNet55),     //I2
                                  .CSALUI8(s_logisimNet12),     //I3
@@ -362,3 +389,4 @@ module CYC_36(
                                  );
 
 endmodule
+
