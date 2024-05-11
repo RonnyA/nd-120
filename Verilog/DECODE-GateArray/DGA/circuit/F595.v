4cc9ee35daa10a4921a983cbf168da2ee37880cc
Ronny Hansen <ronnyha@live.no>
2024-05-11 18:26:24 +0200

    Refactored R/S logic

--- Verilog/DECODE-GateArray/DGA/circuit/F595.v
+++ Verilog/DECODE-GateArray/DGA/circuit/F595.v
@@ -1,92 +1,51 @@
 /******************************************************************************
- ** Logisim-evolution goes FPGA automatic generated Verilog code             **
- ** https://github.com/logisim-evolution/                                    **
  **                                                                          **
  ** Component : F595                                                         **
  **                                                                          **
+ ** R/S Latch with Gated input                                               **
+ **                                                                          **
  *****************************************************************************/
 
-module F595( H01_S,
-             H02_R,
-             H03_G,
-             N01_Q,
-             N02_QB );
+module F595( 
+   input H01_S, // Set
+   input H02_R, // Reset
+   input H03_G, // Gate Enable
 
-   /*******************************************************************************
-   ** The inputs are defined here                                                **
-   *******************************************************************************/
-   input H01_S;
-   input H02_R;
-   input H03_G;
+   output N01_Q, // Q
+   output N02_QB // Qn
+);
 
-   /*******************************************************************************
-   ** The outputs are defined here                                               **
-   *******************************************************************************/
-   output N01_Q;
-   output N02_QB;
+   reg Q_int;
 
-   /*******************************************************************************
-   ** The wires are defined here                                                 **
-   *******************************************************************************/
-   wire s_logisimNet0;
-   wire s_logisimNet1;
-   wire s_logisimNet2;
-   wire s_logisimNet3;
-   wire s_logisimNet4;
-   wire s_logisimNet5;
-   wire s_logisimNet6;
-   wire s_logisimNet7;
-   wire s_logisimNet8;
+   wire s_signal_S;
+   wire s_signal_R;
 
-   /*******************************************************************************
-   ** The module functionality is described here                                 **
-   *******************************************************************************/
+   
+   // G needs to be HIGH to allow S and R to be set
+   assign s_signal_S = H01_S & H03_G;
+   assign s_signal_R = H02_R & H03_G;
 
-   /*******************************************************************************
-   ** Here all input connections are defined                                     **
-   *******************************************************************************/
-   assign s_logisimNet1 = H03_G;
-   assign s_logisimNet3 = H01_S;
-   assign s_logisimNet4 = H02_R;
 
-   /*******************************************************************************
-   ** Here all output connections are defined                                    **
-   *******************************************************************************/
-   assign N01_Q  = s_logisimNet8;
-   assign N02_QB = s_logisimNet6;
+   // Behavioral description of the R-S latch
+   always @(*) begin
+      if (s_signal_R && s_signal_S) begin
+          // Invalid state, typically both Q and Qn should be undefined
+          //Q_int = 1'bx;
+          //Qn_int = 1'bx;
 
-   /*******************************************************************************
-   ** Here all in-lined components are defined                                   **
-   *******************************************************************************/
+         // should be undefined.. but we need to define it 0
+          Q_int = 1'b0;          
 
-   // NOT Gate
-   assign s_logisimNet8 = ~s_logisimNet2;
-
-   // NOT Gate
-   assign s_logisimNet6 = ~s_logisimNet5;
-
-   /*******************************************************************************
-   ** Here all normal components are defined                                     **
-   *******************************************************************************/
-   AND_GATE #(.BubblesMask(2'b00))
-      GATES_1 (.input1(s_logisimNet3),
-               .input2(s_logisimNet1),
-               .result(s_logisimNet0));
-
-   AND_GATE #(.BubblesMask(2'b00))
-      GATES_2 (.input1(s_logisimNet1),
-               .input2(s_logisimNet4),
-               .result(s_logisimNet7));
-
-   NOR_GATE #(.BubblesMask(2'b00))
-      GATES_3 (.input1(s_logisimNet0),
-               .input2(s_logisimNet5),
-               .result(s_logisimNet2));
-
-   NOR_GATE #(.BubblesMask(2'b00))
-      GATES_4 (.input1(s_logisimNet2),
-               .input2(s_logisimNet7),
-               .result(s_logisimNet5));
-
+      end else if (s_signal_R) begin
+          Q_int = 1'b0;          
+      end else if (s_signal_S) begin
+          Q_int = 1'b1;          
+      end /*else begin
+          Q_int = Q_int; // Explicitly maintaining previous state (to avoid warning/error "not all control paths of combinational always assign a value")
+      end*/
+  end
+  
+  assign N01_Q = Q_int;
+  assign N02_QB = ~Q_int;
 
 endmodule
