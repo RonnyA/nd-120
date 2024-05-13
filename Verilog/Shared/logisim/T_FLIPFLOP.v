53f70af21fe341e3cb0cdcea491de4daf699d9cd
Ronny Hansen <ronnyha@live.no>
2024-05-13 23:56:27 +0200

    Flipped priority to Reset (in case of both re-set and set signal applied)

--- Verilog/Shared/logisim/T_FLIPFLOP.v
+++ Verilog/Shared/logisim/T_FLIPFLOP.v
@@ -74,9 +74,9 @@ module T_FLIPFLOP( clock,
    *******************************************************************************/
    //always @(posedge reset or posedge preset or posedge s_clock) //Vivado doesnt like ASYNC RESET )
    always @(posedge s_clock) //Vivado doesnt like ASYNC RESET )
-   begin
-      if (preset) s_currentState <= 1'b1;  // priority to pre-set
-      else if (reset) s_currentState <= 1'b0;      
+   begin      
+      if (reset) s_currentState <= 1'b0;       // priority to re-set
+      else if (preset) s_currentState <= 1'b1;  
       else if (tick) s_currentState <= s_nextState;
    end
 
