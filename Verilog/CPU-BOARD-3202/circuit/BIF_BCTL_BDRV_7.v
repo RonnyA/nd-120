539b68a2ad6e305e599d7b4d719118d53b2608f9
Ronny Hansen <ronnyha@live.no>
2024-05-15 19:44:54 +0200

    Refactored wire-names and connection to CHIP_3A

--- Verilog/CPU-BOARD-3202/circuit/BIF_BCTL_BDRV_7.v
+++ Verilog/CPU-BOARD-3202/circuit/BIF_BCTL_BDRV_7.v
@@ -1,273 +1,226 @@
-/******************************************************************************
- ** Logisim-evolution goes FPGA automatic generated Verilog code             **
- ** https://github.com/logisim-evolution/                                    **
- **                                                                          **
- ** Component : BIF_BCTL_BDRV_7                                              **
- **                                                                          **
- *****************************************************************************/
+/**************************************************************************
+** ND120 CPU, MM&M                                                       **
+** BIF/BCTL/BDRV                                                         **
+** BUS DRIVER                                                            **
+** SHEET 7 of 50                                                         **
+**                                                                       ** 
+** Last reviewed: 13-MAY-2024                                            **
+** Ronny Hansen                                                          **               
+***************************************************************************/
+module BIF_BCTL_BDRV_7
+( 
+   input APR_n,
+   input BDRY25_n,
+   input BDRY50_n,
+   input BINPUT75_n,
+   input CACT_n,
+   input CBWRITE_n,
+   input DAP_n,
+   input EIOD_n,
+   input GNT50_n,
+   input IBDRY_n,
+   input IBREQ_n,
+   input IOD_n,
+   input MEM_n,
+   input MIS0,
+   input REF_n,
+   input SEM_n,
+   input SSEMA_n,
+   input TOUT,
 
-module BIF_BCTL_BDRV_7( APR_n,
-                        BAPR_n,
-                        BDAP_n,
-                        BDRY25_n,
-                        BDRY50_n,
-                        BDRY_n,
-                        BERROR_n,
-                        BINACK_n,
-                        BINPUT75_n,
-                        BINPUT_n,
-                        BIOXE_n,
-                        BMEM_n,
-                        BREF_n,
-                        CACT_n,
-                        CBWRITE_n,
-                        DAP_n,
-                        EIOD_n,
-                        GNT50_n,
-                        IBDRY_n,
-                        IBREQ_n,
-                        IOD_n,
-                        IOXERR_n,
-                        MEM_n,
-                        MIS0,
-                        MOR_n,
-                        OUTGRANT_n,
-                        OUTIDENT_n,
-                        REF_n,
-                        SEMRQ_n,
-                        SEM_n,
-                        SSEMA_n,
-                        TOUT );
+   output BAPR_n,
+   output BDAP_n,
+   output BDRY_n,
+   output BERROR_n,
+   output BINACK_n,
+   output BINPUT_n,
+   output BIOXE_n,
+   output BMEM_n,
+   output BREF_n,
+   output IOXERR_n,
+   output MOR_n,
+   output OUTGRANT_n,
+   output OUTIDENT_n,
+   output SEMRQ_n
+);
 
-   /*******************************************************************************
-   ** The inputs are defined here                                                **
-   *******************************************************************************/
-   input APR_n;
-   input BDRY25_n;
-   input BDRY50_n;
-   input BINPUT75_n;
-   input CACT_n;
-   input CBWRITE_n;
-   input DAP_n;
-   input EIOD_n;
-   input GNT50_n;
-   input IBDRY_n;
-   input IBREQ_n;
-   input IOD_n;
-   input MEM_n;
-   input MIS0;
-   input REF_n;
-   input SEM_n;
-   input SSEMA_n;
-   input TOUT;
-
-   /*******************************************************************************
-   ** The outputs are defined here                                               **
-   *******************************************************************************/
-   output BAPR_n;
-   output BDAP_n;
-   output BDRY_n;
-   output BERROR_n;
-   output BINACK_n;
-   output BINPUT_n;
-   output BIOXE_n;
-   output BMEM_n;
-   output BREF_n;
-   output IOXERR_n;
-   output MOR_n;
-   output OUTGRANT_n;
-   output OUTIDENT_n;
-   output SEMRQ_n;
 
    /*******************************************************************************
    ** The wires are defined here                                                 **
    *******************************************************************************/
-   wire s_logisimNet0;
-   wire s_logisimNet1;
-   wire s_logisimNet10;
-   wire s_logisimNet11;
-   wire s_logisimNet12;
-   wire s_logisimNet13;
-   wire s_logisimNet14;
-   wire s_logisimNet15;
-   wire s_logisimNet16;
-   wire s_logisimNet17;
-   wire s_logisimNet18;
-   wire s_logisimNet19;
-   wire s_logisimNet2;
-   wire s_logisimNet20;
-   wire s_logisimNet21;
-   wire s_logisimNet22;
-   wire s_logisimNet23;
-   wire s_logisimNet24;
-   wire s_logisimNet25;
-   wire s_logisimNet26;
-   wire s_logisimNet27;
-   wire s_logisimNet28;
-   wire s_logisimNet29;
-   wire s_logisimNet3;
-   wire s_logisimNet30;
-   wire s_logisimNet31;
-   wire s_logisimNet32;
-   wire s_logisimNet33;
-   wire s_logisimNet34;
-   wire s_logisimNet35;
-   wire s_logisimNet36;
-   wire s_logisimNet37;
-   wire s_logisimNet38;
-   wire s_logisimNet39;
-   wire s_logisimNet4;
-   wire s_logisimNet40;
-   wire s_logisimNet41;
-   wire s_logisimNet5;
-   wire s_logisimNet6;
-   wire s_logisimNet7;
-   wire s_logisimNet8;
-   wire s_logisimNet9;
-
-   /*******************************************************************************
-   ** The module functionality is described here                                 **
-   *******************************************************************************/
+   wire s_iod_n;
+   wire s_eiod_n;
+   wire s_outident_n;
+   wire s_bioxe_n;
+   wire s_binack_n;
+   wire s_ioxerr_n;
+   wire s_mor_n;
+   wire s_bdry_n;
+   wire s_ibdry_n;
+   wire s_sem_n;
+   wire s_cbwrite_n;
+   
+   wire s_out_8g_6;   
+   wire s_gnt50;
+   wire s_gnd;
+   wire s_mis0_n;
+   wire s_dap_n;
+   wire s_ssema_n;
+   wire s_cact_n;
+   wire s_bmem_n;
+   wire s_mem_n;
+   wire s_out_2a_8;
+   wire s_berror_n;
+   wire s_gnt50_n;
+   wire s_out_2b_8;
+   wire s_ibreq;
+   wire s_out_8g_3;
+   wire s_ref_n;
+   wire s_apr_n;
+   wire s_ibreq_n;
+   wire s_bdry25_n;
+   wire s_bdry50_n;
+   wire s_bdap_n;
+   wire s_binput75_n;
+   wire s_out_2b_3;
+   wire s_tout;   
+   wire s_dap;
+   wire s_out_9d_10;
+   wire s_mis0;
+   wire s_out_2b_6;
 
    /*******************************************************************************
    ** Here all input connections are defined                                     **
    *******************************************************************************/
-   assign s_logisimNet0  = IOD_n;
-   assign s_logisimNet1  = EIOD_n;
-   assign s_logisimNet16 = IBDRY_n;
-   assign s_logisimNet17 = SEM_n;
-   assign s_logisimNet24 = DAP_n;
-   assign s_logisimNet25 = SSEMA_n;
-   assign s_logisimNet26 = CACT_n;
-   assign s_logisimNet28 = MEM_n;
-   assign s_logisimNet30 = GNT50_n;
-   assign s_logisimNet34 = REF_n;
-   assign s_logisimNet35 = APR_n;
-   assign s_logisimNet36 = IBREQ_n;
-   assign s_logisimNet37 = BDRY25_n;
-   assign s_logisimNet38 = BDRY50_n;
-   assign s_logisimNet4  = BINPUT75_n;
-   assign s_logisimNet41 = TOUT;
-   assign s_logisimNet5  = CBWRITE_n;
-   assign s_logisimNet8  = MIS0;
+   assign s_apr_n          = APR_n;
+   assign s_bdry25_n       = BDRY25_n;
+   assign s_bdry50_n       = BDRY50_n;
+   assign s_binput75_n     = BINPUT75_n;
+   assign s_cact_n         = CACT_n;
+   assign s_cbwrite_n      = CBWRITE_n;
+   assign s_dap_n          = DAP_n;
+   assign s_gnt50_n        = GNT50_n;
+   assign s_ibdry_n        = IBDRY_n;
+   assign s_ibreq_n        = IBREQ_n;
+   assign s_iod_n          = IOD_n;
+   assign s_eiod_n         = EIOD_n;
+   assign s_mem_n          = MEM_n;
+   assign s_mis0           = MIS0;
+   assign s_ref_n          = REF_n;
+   assign s_sem_n          = SEM_n;
+   assign s_ssema_n        = SSEMA_n;
+   assign s_tout           = TOUT;
 
    /*******************************************************************************
    ** Here all output connections are defined                                    **
    *******************************************************************************/
-   assign BAPR_n     = s_logisimNet20;
-   assign BDAP_n     = s_logisimNet39;
-   assign BDRY_n     = s_logisimNet15;
-   assign BERROR_n   = s_logisimNet3;
-   assign BINACK_n   = s_logisimNet12;
-   assign BINPUT_n   = s_logisimNet18;
-   assign BIOXE_n    = s_logisimNet11;
-   assign BMEM_n     = s_logisimNet27;
-   assign BREF_n     = s_logisimNet19;
-   assign IOXERR_n   = s_logisimNet13;
-   assign MOR_n      = s_logisimNet14;
-   assign OUTGRANT_n = s_logisimNet31;
-   assign OUTIDENT_n = s_logisimNet10;
-   assign SEMRQ_n    = s_logisimNet29;
+   assign BAPR_n     = s_apr_n;
+   assign BDAP_n     = s_bdap_n;
+   assign BDRY_n     = s_bdry_n;
+   assign BERROR_n   = s_berror_n;
+   assign BINACK_n   = s_binack_n;
+   assign BINPUT_n   = s_cbwrite_n;
+   assign BIOXE_n    = s_bioxe_n;
+   assign BMEM_n     = s_bmem_n;
+   assign BREF_n     = s_ref_n;
+   assign IOXERR_n   = s_ioxerr_n;
+   assign MOR_n      = s_mor_n;
+   assign OUTGRANT_n = s_out_2b_8;
+   assign OUTIDENT_n = s_outident_n;
+   assign SEMRQ_n    = s_out_2a_8;
 
    /*******************************************************************************
    ** Here all in-lined components are defined                                   **
    *******************************************************************************/
 
    // Ground
-   assign  s_logisimNet22  =  1'b0;
+   assign  s_gnd  =  1'b0;
 
-
-   // NOT Gate
-   assign s_logisimNet21 = ~s_logisimNet30;
-
-   // NOT Gate
-   assign s_logisimNet23 = ~s_logisimNet8;
-
-   // NOT Gate
-   assign s_logisimNet32 = ~s_logisimNet36;
-
-   // NOT Gate
-   assign s_logisimNet6 = ~s_logisimNet24;
-
-   // Buffer
-   assign s_logisimNet18 = s_logisimNet5;
-
-   // Buffer
-   assign s_logisimNet19 = s_logisimNet34;
-
-   // Buffer
-   assign s_logisimNet20 = s_logisimNet35;
+   // Signal inverters
+   assign s_gnt50    = ~s_gnt50_n;
+   assign s_mis0_n   = ~s_mis0;
+   assign s_ibreq    = ~s_ibreq_n;
+   assign s_dap      = ~s_dap_n;
 
    /*******************************************************************************
    ** Here all normal components are defined                                     **
    *******************************************************************************/
    OR_GATE #(.BubblesMask(2'b11))
-      GATES_1 (.input1(s_logisimNet40),
-               .input2(s_logisimNet17),
-               .result(s_logisimNet9));
+      GATES_1 (.input1(s_out_2b_3),
+               .input2(s_sem_n),
+               .result(s_out_2b_6));
 
    NAND_GATE #(.BubblesMask(2'b00))
-      GATES_2 (.input1(s_logisimNet5),
-               .input2(s_logisimNet7),
-               .result(s_logisimNet29));
+      GATES_2 (.input1(s_cbwrite_n),
+               .input2(s_out_9d_10),
+               .result(s_out_2a_8));
 
    NAND_GATE #(.BubblesMask(2'b00))
-      GATES_3 (.input1(s_logisimNet9),
-               .input2(s_logisimNet21),
-               .result(s_logisimNet31));
+      GATES_3 (.input1(s_out_2b_6),
+               .input2(s_gnt50),
+               .result(s_out_2b_8));
 
    NAND_GATE #(.BubblesMask(2'b00))
-      GATES_4 (.input1(s_logisimNet32),
-               .input2(s_logisimNet0),
-               .result(s_logisimNet33));
+      GATES_4 (.input1(s_ibreq),
+               .input2(s_iod_n),
+               .result(s_out_8g_3));
 
    OR_GATE #(.BubblesMask(2'b11))
-      GATES_5 (.input1(s_logisimNet33),
-               .input2(s_logisimNet28),
-               .result(s_logisimNet2));
+      GATES_5 (.input1(s_out_8g_3),
+               .input2(s_mem_n),
+               .result(s_out_8g_6));
 
    AND_GATE #(.BubblesMask(2'b11))
-      GATES_6 (.input1(s_logisimNet25),
-               .input2(s_logisimNet26),
-               .result(s_logisimNet7));
+      GATES_6 (.input1(s_ssema_n),
+               .input2(s_cact_n),
+               .result(s_out_9d_10));
 
    NAND_GATE #(.BubblesMask(2'b00))
-      GATES_7 (.input1(s_logisimNet6),
-               .input2(s_logisimNet38),
-               .result(s_logisimNet39));
+      GATES_7 (.input1(s_dap),
+               .input2(s_bdry50_n),
+               .result(s_bdap_n));
 
    NAND_GATE #(.BubblesMask(2'b00))
-      GATES_8 (.input1(s_logisimNet16),
-               .input2(s_logisimNet2),
-               .result(s_logisimNet27));
+      GATES_8 (.input1(s_ibdry_n),
+               .input2(s_out_8g_6),
+               .result(s_bmem_n));
 
    NAND_GATE #(.BubblesMask(2'b00))
-      GATES_9 (.input1(s_logisimNet37),
-               .input2(s_logisimNet38),
-               .result(s_logisimNet40));
+      GATES_9 (.input1(s_bdry25_n),
+               .input2(s_bdry50_n),
+               .result(s_out_2b_3));
 
 
    /*******************************************************************************
    ** Here all sub-circuits are defined                                          **
    *******************************************************************************/
+   
+   wire [3:0] s_A1 = {1'b0, s_binput75_n, s_mis0_n, s_mis0};
+   wire [3:0] s_Y1;
 
-   TTL_74241   CHIP_3A (.I0_1A1(s_logisimNet8),
-                        .I1_1A2(s_logisimNet23),
-                        .I2_1A3(s_logisimNet4),
-                        .I3_1A4(1'b0),
-                        .I4_2A1(s_logisimNet0),
-                        .I5_2A2(s_logisimNet28),
-                        .I6_2A3(s_logisimNet22),
-                        .I7_2A4(s_logisimNet3),
-                        .O0_1Y1(s_logisimNet10),
-                        .O1_1Y2(s_logisimNet11),
-                        .O2_1Y3(s_logisimNet12),
-                        .O3_1Y4(),
-                        .O4_2Y1(s_logisimNet13),
-                        .O5_2Y2(s_logisimNet14),
-                        .O6_2Y3(s_logisimNet3),
-                        .O7_2Y4(s_logisimNet15),
-                        .OE1_1G_n(s_logisimNet1),
-                        .OE2_2G(s_logisimNet41));
+   assign s_outident_n = s_eiod_n ? 1'b1 : s_Y1[0]; // pull OUTIDENT_n high if EIOD_n is high (giving 3 state output on 74241-Y1)
+   assign s_bioxe_n    = s_Y1[1]; 
+   assign s_binack_n   = s_Y1[2];
+   // 4th output of 74241-Y1 is not connected
+
+   wire [3:0] s_A2 = {s_berror_n, s_gnd, s_mem_n, s_iod_n};
+   wire [3:0] s_Y2;
+
+   assign s_ioxerr_n       = s_tout ? s_Y2[0] : 1'b1; // pull IOXERR_n high if TOUT is low (giving 3 state output on 74241-Y2)
+   assign s_mor_n          = s_tout ? s_Y2[1] : 1'b1;    // pull MOR_n high if TOUT is low (giving 3 state output on 74241-Y2)
+   assign s_berror_n       = s_Y2[2];
+   assign s_bdry_n         = s_Y2[3];
+   
+
+   TTL_74241   CHIP_3A 
+   (
+      .A1(s_A1[3:0]),
+      .Y1(s_Y1),
+      .G1_n(s_eiod_n),
+
+      .A2(s_A2[3:0]),
+      .Y2(s_Y2),
+      .G2(s_tout)
+   );
 
 endmodule
