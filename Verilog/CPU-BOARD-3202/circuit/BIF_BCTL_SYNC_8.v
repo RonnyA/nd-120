32344f52cbf7c7dd7b224fb7ce6f84104fdac74c
Ronny Hansen <ronnyha@live.no>
2024-05-15 21:26:34 +0200

    Refactored wire-signal names

--- Verilog/CPU-BOARD-3202/circuit/BIF_BCTL_SYNC_8.v
+++ Verilog/CPU-BOARD-3202/circuit/BIF_BCTL_SYNC_8.v
@@ -2,7 +2,7 @@
 ** ND120 CPU, MM&M                                                       **
 ** BIF/BCTL/SYNC                                                         **
 ** BIF SYNC                                                              **
-** SHEET 8  of 50                                                        **
+** SHEET 8 of 50                                                         **
 **                                                                       ** 
 ** Last reviewed: 21-APRIL-2024                                          **
 ** Ronny Hansen                                                          **               
@@ -41,163 +41,112 @@ module BIF_BCTL_SYNC_8(
    output SEMRQ50_n
 );
 
-
-
    /*******************************************************************************
    ** The wires are defined here                                                 **
    *******************************************************************************/
-   wire s_logisimNet0;
-   wire s_logisimNet1;
-   wire s_logisimNet10;
-   wire s_logisimNet11;
-   wire s_logisimNet12;
-   wire s_logisimNet13;
-   wire s_logisimNet14;
-   wire s_logisimNet15;
-   wire s_logisimNet16;
+   wire s_bdry25_n;
+   wire s_binput50_n;
+   wire s_osc;
+   wire s_pd1;
+   wire s_pd3;
+   wire s_ibreq_n;
+   wire s_block_n;
+   wire s_cact_n;
+   wire s_ibperr_n;
+   wire s_isemrq_n;
+   wire s_clear_n;
+   wire s_refrq_n;
+   wire s_ibinput_n;
+   wire s_ibdry_n;
    wire s_ibdap_n;
-   wire s_logisimNet18;
-   wire s_logisimNet19;
-   wire s_logisimNet2;
-   wire s_logisimNet20;
-   wire s_logisimNet21;
-   wire s_logisimNet22;
-   wire s_logisimNet23;
-   wire s_logisimNet24;
-   wire s_logisimNet25;
-   wire s_logisimNet26;
-   wire s_logisimNet27;
-   wire s_logisimNet28;
-   wire s_logisimNet29;
-   wire s_logisimNet3;
-   wire s_logisimNet30;
-   wire s_logisimNet31;
-   wire s_logisimNet32;
-   wire s_logisimNet4;
-   wire s_logisimNet5;
-   wire s_logisimNet6;
-   wire s_logisimNet7;
-   wire s_logisimNet8;
-   wire s_logisimNet9;
+   wire s_binput75_n;
+   wire s_breq50_n;
+   wire s_bdap50_n;
+   wire s_bdry50_n;
+   wire s_bdry75_n;
+   wire s_block25_n;
+   wire s_cact25_n;
+   wire s_mr_n;
+   wire s_refrq50_n;
+   wire s_semrq50_n;
+   wire s_bperr50_n;
 
+   // Intermediate wires for CHIP_3D outputs
+   wire s_bdap25_n;
+   wire s_breq25_n;
+   wire s_bperr25_n;
+   wire s_binput25_n;
+   wire s_semrq25_n;
+   wire s_refrq25_n;
+   wire s_clear25_n;
+
+   // Intermediate wires for CHIP_3D and CHIP_4D inputs and outputs
    wire [9:0] input_chip_3d;
    wire [9:0] output_chip_3d;
-
-
    wire [9:0] input_chip_4d;
    wire [9:0] output_chip_4d;
-
-
-   /*******************************************************************************
-   ** The module functionality is described here                                 **
-   *******************************************************************************/
 
    /*******************************************************************************
    ** Here all input connections are defined                                     **
    *******************************************************************************/
-   assign s_ibdap_n = IBDAP_n;
-   assign s_logisimNet18 = BLOCK_n;
-   assign s_logisimNet19 = CACT_n;
-   assign s_logisimNet2  = OSC;
-   assign s_logisimNet20 = IBPERR_n;
-   assign s_logisimNet21 = ISEMRQ_n;
-   assign s_logisimNet22 = CLEAR_n;
-   assign s_logisimNet23 = REFRQ_n;
-   assign s_logisimNet24 = IBINPUT_n;
-   assign s_logisimNet25 = IBDRY_n;
-   assign s_logisimNet3  = PD1;
-   assign s_logisimNet5  = PD3;
-   assign s_logisimNet7  = IBREQ_n;
+   assign s_ibdap_n  = IBDAP_n;
+   assign s_block_n  = BLOCK_n;
+   assign s_cact_n   = CACT_n;
+   assign s_osc      = OSC;
+   assign s_ibperr_n = IBPERR_n;
+   assign s_isemrq_n = ISEMRQ_n;
+   assign s_clear_n  = CLEAR_n;
+   assign s_refrq_n  = REFRQ_n;
+   assign s_ibinput_n= IBINPUT_n;
+   assign s_ibdry_n  = IBDRY_n;
+   assign s_pd1      = PD1;
+   assign s_pd3      = PD3;
+   assign s_ibreq_n  = IBREQ_n;
 
    /*******************************************************************************
    ** Here all output connections are defined                                    **
    *******************************************************************************/
-   assign BDAP50_n   = s_logisimNet11;
-   assign BDRY25_n   = s_logisimNet0;
-   assign BDRY50_n   = s_logisimNet4;
-   assign BDRY75_n   = s_logisimNet9;
-   assign BINPUT50_n = s_logisimNet1;
-   assign BINPUT75_n = s_logisimNet12;
-   assign BLOCK25_n  = s_logisimNet16;
-   assign BPERR50_n  = s_logisimNet8;
-   assign BREQ50_n   = s_logisimNet10;
-   assign CACT25_n   = s_logisimNet6;
-   assign MR_n       = s_logisimNet15;
-   assign REFRQ50_n  = s_logisimNet13;
-   assign SEMRQ50_n  = s_logisimNet14;
+   assign BDAP50_n   = s_bdap50_n;
+   assign BDRY25_n   = s_bdry25_n;
+   assign BDRY50_n   = s_bdry50_n;
+   assign BDRY75_n   = s_bdry75_n;
+   assign BINPUT50_n = s_binput50_n;
+   assign BINPUT75_n = s_binput75_n;
+   assign BLOCK25_n  = s_block25_n;
+   assign BPERR50_n  = s_bperr50_n;
+   assign BREQ50_n   = s_breq50_n;
+   assign CACT25_n   = s_cact25_n;
+   assign MR_n       = s_mr_n;
+   assign REFRQ50_n  = s_refrq50_n;
+   assign SEMRQ50_n  = s_semrq50_n;
 
    /*******************************************************************************
    ** Here all sub-circuits are defined                                          **
    *******************************************************************************/
 
+   // Bus Driver 10 bits. Positive edge-triggered registers with D-type flip-flops and 3-state
+   AM29C821 CHIP_3D (
+      .CK(s_osc),
+      .OE_n(s_pd3),
+      .D(input_chip_3d),
+      .Y(output_chip_3d)          
+   );
 
-   /* TODO ASSSIGN
-             .D0(s_logisimNet22),
-                       .D1(s_logisimNet23),
-                       .D2(s_logisimNet21),
-                       .D3(s_logisimNet24),
-                       .D4(s_logisimNet20),
-                       .D5(s_logisimNet25),
-                       .D6(s_logisimNet19),
-                       .D7(s_logisimNet7),
-                       .D8(s_ibdap_n),
-                       .D9(s_logisimNet18),
-                       
-                       .Y0(s_logisimNet26),
-                       .Y1(s_logisimNet27),
-                       .Y2(s_logisimNet28),
-                       .Y3(s_logisimNet29),
-                       .Y4(s_logisimNet30),
-                       .Y5(s_logisimNet0),
-                       .Y6(s_logisimNet6),
-                       .Y7(s_logisimNet31),
-                       .Y8(s_logisimNet32),
-                       .Y9(s_logisimNet16)
+   // Assignments for CHIP_3D inputs and outputs
+   assign input_chip_3d = {s_block_n,   s_ibdap_n,  s_ibreq_n,  s_cact_n,   s_ibdry_n,  s_ibperr_n,  s_ibinput_n,  s_isemrq_n,  s_refrq_n,   s_clear_n   };
+   assign                 {s_block25_n, s_bdap25_n, s_breq25_n, s_cact25_n, s_bdry25_n, s_bperr25_n, s_binput25_n, s_semrq25_n, s_refrq25_n, s_clear25_n } = output_chip_3d;
+      
 
-*/
+   // Bus Driver 10 bits. Positive edge-triggered registers with D-type flip-flops and 3-state
+   AM29C821 CHIP_4D (
+      .CK(s_osc),
+      .OE_n(s_pd1),
+      .D(input_chip_4d),
+      .Y(output_chip_4d)
+   );
 
-      //  Bus Driver 10 bits. Positive edge-triggered registeres with D-type flip-flops and 3-state                       
-   AM29C821   CHIP_3D (
-                        .CK(s_logisimNet2),
-                        .OE_n(s_logisimNet5),
-                        .D(input_chip_3d),
-                        .Y(output_chip_3d)          
-                       );
-
-
-                       /* TODO ASSSIGN
-
-                       .D0(s_logisimNet26),
-                       .D1(s_logisimNet27),
-                       .D2(s_logisimNet28),
-                       .D3(s_logisimNet29),
-                       .D4(s_logisimNet30),
-                       .D5(s_logisimNet0),
-                       .D6(s_logisimNet4),
-                       .D7(s_logisimNet31),
-                       .D8(s_logisimNet32),
-                       .D9(s_logisimNet1),
-                       
-                       .Y0(s_logisimNet15),
-                       .Y1(s_logisimNet13),
-                       .Y2(s_logisimNet14),
-                       .Y3(s_logisimNet1),
-                       .Y4(s_logisimNet8),
-                       .Y5(s_logisimNet4),
-                       .Y6(s_logisimNet9),
-                       .Y7(s_logisimNet10),
-                       .Y8(s_logisimNet11),
-                       .Y9(s_logisimNet12)
-                       */
-
-
-   //  Bus Driver 10 bits. Positive edge-triggered registeres with D-type flip-flops and 3-state
-   AM29C821   CHIP_4D (
-                        .CK(s_logisimNet2),
-                        .OE_n(s_logisimNet3),
-                        .D(input_chip_4d),
-                        .Y(output_chip_4d)
-                       );
-
+   // Assignments for CHIP_4D inputs and outputs
+   assign input_chip_4d = {s_binput50_n, s_bdap25_n, s_breq25_n, s_bdry50_n, s_bdry25_n, s_bperr25_n, s_binput25_n, s_semrq25_n, s_refrq25_n, s_clear25_n};
+   assign                 {s_binput75_n, s_bdap50_n, s_breq50_n, s_bdry75_n, s_bdry50_n, s_bperr50_n, s_binput50_n, s_semrq50_n, s_refrq50_n, s_mr_n } = output_chip_4d;
 
 endmodule
