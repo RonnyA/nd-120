044a347c945388bc536d5fc39c78c71f029ce32a
Ronny Hansen <ronnyha@live.no>
2024-05-15 21:25:28 +0200

    Refactored wire-signal names

--- Verilog/CPU-BOARD-3202/circuit/IO_PANCAL_40.v
+++ Verilog/CPU-BOARD-3202/circuit/IO_PANCAL_40.v
@@ -45,57 +45,25 @@ module IO_PANCAL_40(
    ** The wires are defined here                                                 **
    *******************************************************************************/
    wire [4:0]  s_dp_5_1_n;
-   wire [4:0]  s_logisimBus2;
-   wire [3:0]  s_logisimBus20;
+   wire [4:0]  s_ground_bus;
+   wire [3:0]  s_bus_4_bit;
    wire [1:0]  s_stat_4_3;
    wire [1:0]  s_pcr_1_0;
-   wire [15:0] s_IDB_15_0_out;
+   wire [15:0] s_idb_15_0_out;
    wire [7:0]  s_pa_7_0;
-   wire        s_logisimNet0;
-   wire        s_logisimNet1;
-   wire        s_logisimNet11;
-   wire        s_logisimNet12;
-   wire        s_logisimNet13;
-   wire        s_logisimNet14;
-   wire        s_logisimNet15;
-   wire        s_logisimNet16;
-   wire        s_logisimNet17;
-   wire        s_logisimNet18;
-   wire        s_logisimNet21;
-   wire        s_logisimNet22;
-   wire        s_logisimNet23;
-   wire        s_logisimNet24;
-   wire        s_logisimNet25;
-   wire        s_logisimNet26;
-   wire        s_logisimNet27;
-   wire        s_logisimNet28;
-   wire        s_logisimNet29;
-   wire        s_logisimNet31;
-   wire        s_logisimNet32;
-   wire        s_logisimNet33;
-   wire        s_logisimNet34;
-   wire        s_logisimNet35;
-   wire        s_logisimNet36;
-   wire        s_logisimNet37;
-   wire        s_logisimNet38;
-   wire        s_logisimNet39;
-   wire        s_logisimNet4;
-   wire        s_logisimNet40;
-   wire        s_logisimNet41;
-   wire        s_logisimNet42;
-   wire        s_logisimNet43;
+   wire        s_ful_n;
+   wire        s_clear_n;
+   wire        s_poni;
+   wire        s_val;
+   wire        s_epans;
+   wire        s_ioni;
+   wire        s_lhit;
+   wire        s_lev0;
+   wire        s_emp_n;
+   wire        s_panos;
    wire        s_ck;
-   wire        s_logisimNet45;
-   wire        s_EPANS;
    wire        s_rmm_n;
-   wire        s_logisimNet5;
-   wire        s_logisimNet6;
-   wire        s_logisimNet7;
-   wire        s_logisimNet8;
-   wire        s_logisimNet9;
-
-   wire [7:0] s_IDB_7_0_chip32_out;
-
+   wire [7:0]  s_idb_7_0_chip32_out;
    /*******************************************************************************
    ** The module functionality is described here                                 **
    *******************************************************************************/
@@ -103,58 +71,61 @@ module IO_PANCAL_40(
    /*******************************************************************************
    ** Here all input connections are defined                                     **
    *******************************************************************************/
-   assign s_pcr_1_0[1:0]      = PCR_1_0;
-   assign s_pa_7_0[7:0]       = PA_7_0;
-   assign s_logisimNet29      = FUL_n;
-   assign s_logisimNet39      = CLEAR_n;
-   assign s_logisimNet4       = PONI;
-   assign s_logisimNet45      = VAL;
-   assign s_EPANS             = EPANS;
-   assign s_logisimNet5       = IONI;
-   assign s_logisimNet6       = LHIT;
-   assign s_logisimNet7       = LEV0;
-   assign s_logisimNet8       = EMP_n;
-   assign s_logisimNet9       = PANOSC;
+   assign s_pcr_1_0         = PCR_1_0;
+   assign s_pa_7_0          = PA_7_0;
+   assign s_ful_n           = FUL_n;
+   assign s_clear_n         = CLEAR_n;
+   assign s_poni            = PONI;
+   assign s_val             = VAL;
+   assign s_epans           = EPANS;
+   assign s_ioni            = IONI;
+   assign s_lhit            = LHIT;
+   assign s_lev0            = LEV0;
+   assign s_emp_n           = EMP_n;
+   assign s_panos           = PANOSC;
 
    /*******************************************************************************
    ** Here all output connections are defined                                    **
    *******************************************************************************/
-   assign DP_5_1_n         = s_dp_5_1_n[4:0];
-   assign IDB_15_0_OUT     = s_IDB_15_0_out[15:0];
+   assign DP_5_1_n         = s_dp_5_1_n;
+   assign IDB_15_0_OUT     = s_idb_15_0_out;
    assign RMM_n            = s_rmm_n;
-   assign STAT_4_3         = s_stat_4_3[1:0];
+   assign STAT_4_3         = s_stat_4_3;
 
    /*******************************************************************************
    ** Here all in-lined components are defined                                   **
    *******************************************************************************/
 
    // Ground
-   assign  s_logisimBus2[4:0]  =  {1'b0, 4'h0};
-
+   assign s_ground_bus = 5'b00000;
 
    // NOT Gate
-   assign s_dp_5_1_n = ~s_logisimBus2;
+   assign s_dp_5_1_n = ~s_ground_bus; // Signals coming from 68705 - add in logic later
 
    /*******************************************************************************
    ** Here all sub-circuits are defined                                          **
    *******************************************************************************/
 
+   wire s_wmm_n;
+   assign s_wmm_n = 0;
+ 
+   // TTL_74374 CHIP_32B
+   TTL_74374 CHIP_32B(
+      .CK(s_wmm_n), // from 68705
+      .OE_n(s_epans),
+      .D(s_pa_7_0),
+      .Q(s_idb_7_0_chip32_out)
+   );
 
-   // TTL_74244 CHIP_33B (simplified..)
-   assign s_IDB_15_0_out[15:8] = s_EPANS  ? 8'b0 : {s_logisimNet1,s_logisimNet29,s_logisimNet0,s_logisimNet45,s_logisimBus20[3:1]};
 
-   
-// TTL_74374 CHIP_32B 
-   TTL_74374   CHIP_32B(
-                          .CK(s_ck),
-                          .OE_n(s_EPANS),
-                          .D(s_pa_7_0[7:0]),
-                          .Q(s_IDB_7_0_chip32_out[7:0])
-                       );
+   //assign s_idb_15_0_out[15:8] = s_epans ? 8'b0 : s_pres, s_ful_n, s_read, s_val, s_stat3_, s_stat2, s_stat1, s_stat0};    // Add in when 68705 is implemented
+   assign s_idb_15_0_out[15:0] = s_epans ? 8'b0 : s_idb_7_0_chip32_out;
+
+
 
 // TODO:
-//  ADD  - MM58274  - Real Time Clock
+// ADD - MM58274  - Real Time Clock
 // ADD - MC68705 - 6805 Embedded CPU
 
-   assign IDB_15_0_OUT = 16'b0;  //TODO: or in s_IDB_7_0_chip32_out[7:0] 
+   
 endmodule
