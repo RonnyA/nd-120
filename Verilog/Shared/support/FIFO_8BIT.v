50a0b3a6fc50682235aecf7af91b14745c6b104f
Ronny Hansen <ronnyha@live.no>
2024-05-20 18:17:23 +0200

    Refactored

--- Verilog/Shared/support/FIFO_8BIT.v
+++ Verilog/Shared/support/FIFO_8BIT.v
@@ -6,8 +6,8 @@ module FIFO_8BIT #(
     input rd_en,              // Read enable
     input [7:0] data_in,      // Data input
     output reg [7:0] data_out,// Data output
-    output full,              // FIFO full flag
-    output empty              // FIFO empty flag
+    output reg full,              // FIFO full flag
+    output reg empty              // FIFO empty flag
 );
 
     /* verilator lint_off LATCH */
@@ -22,45 +22,52 @@ module FIFO_8BIT #(
     reg [ADDR_WIDTH-1:0] rd_ptr; // Read pointer
     reg [ADDR_WIDTH:0] fifo_count; // Counter for number of elements in the FIFO
 
-    // Write operation
-    always @(posedge wr_en) begin
-        if (wr_en && !full) begin
-            mem[wr_ptr] = data_in;
-            wr_ptr = (wr_ptr == (DEPTH - 1)) ? 0 : wr_ptr + 1;
+
+     // Write operation
+    always @(posedge wr_en or posedge rst) begin
+        if (rst) begin
+            wr_ptr <= 0;
+        end else if (wr_en && !full) begin
+            mem[wr_ptr] <= data_in;
+            wr_ptr <= (wr_ptr == (DEPTH - 1)) ? 0 : wr_ptr + 1;
         end
     end
 
-    // Read operation
-    always @(posedge rd_en) begin
-        if (rd_en && !empty) begin
-            data_out = mem[rd_ptr];
+
+      // Read operation
+    always @(posedge rd_en or posedge rst) begin
+        if (rst) begin
+            rd_ptr = 0;
+            data_out <= 8'b0;
+        end else if (rd_en && !empty) begin
+            data_out <= mem[rd_ptr];
+            //rd_ptr <= (rd_ptr == DEPTH - 1) ? 0 : rd_ptr + 1;
+            
             if (rd_ptr == DEPTH - 1)
                 rd_ptr = 0;
             else
                 rd_ptr = rd_ptr + 1;
-
-            //rd_ptr = (rd_ptr == (DEPTH - 1)) ? 0 : rd_ptr + 1;
+            
         end
     end
 
     // FIFO count management
-    always @(*) begin
+    always @(posedge wr_en or posedge rd_en or posedge rst) begin
+    //always @(*) begin
         if (rst) begin
-            fifo_count = 0;
-            wr_ptr = 0;
-            rd_ptr = 0;
-            data_out = 8'b0;
-        end else begin
-            if (wr_en && !rd_en && !full) begin
-                fifo_count = fifo_count + 1;
-            end else if (!wr_en && rd_en && !empty) begin
-                fifo_count = fifo_count - 1;
-            end
+            fifo_count <= 0;
+        end else if (wr_en && !rd_en && !full) begin
+            fifo_count <= fifo_count + 1;
+            
+        end else if (!wr_en && rd_en && !empty) begin
+            fifo_count <= fifo_count - 1;
         end
+
     end
 
-    // Full and empty flags
-    assign full = (fifo_count == DEPTH);
-    assign empty = (fifo_count == 0);
+    always @(*) begin
+        full = (fifo_count == DEPTH);            
+        empty = (fifo_count == 0);
+    end
 
 endmodule
