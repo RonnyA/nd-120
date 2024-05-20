d19ca588888ba806175f0beb0177345a0332a432
Ronny Hansen <ronnyha@live.no>
2024-05-20 18:53:25 +0200

    Refactored wire-signal names

--- Verilog/CPU-BOARD-3202/circuit/CPU_CS_ACAL_17.v
+++ Verilog/CPU-BOARD-3202/circuit/CPU_CS_ACAL_17.v
@@ -23,19 +23,19 @@ module CPU_CS_ACAL_17(
    /*******************************************************************************
    ** The wires are defined here                                                 **
    *******************************************************************************/
-   wire [9:0]  s_UUA_chip_31g;
-   wire [9:0]  s_UUA_chip_32g;
-   wire [9:0]  s_CSCA_9_0;
-   wire [7:0]  q_lua_12_7; // output from Q_7_0 on chip 30H
-   wire [12:0] s_CSA_12_0;
-   wire [12:0] s_LUA;
-   wire [11:0] s_UUA;
-   wire [7:0]  s_D_chip30h;
-   wire        s_PD1;
-   wire        s_LUA12;
-   wire        s_LUA12_n;
-   wire        s_CLK;
-   wire        s_MACLK;
+   wire [9:0]  s_uua_chip_31g_9_0;
+   wire [9:0]  s_uua_chip_32g_9_0;
+   wire [9:0]  s_csca_9_0;
+   wire [7:0]  s_q_chip30h_7_0; // output from Q_7_0 on chip 30H
+   wire [12:0] s_csa_12_0;
+   wire [12:0] s_lua;
+   wire [11:0] s_uua;
+   wire [7:0]  s_d_chip30h_7_0;
+   wire        s_pd1;
+   wire        s_lua12;
+   wire        s_lua12_n;
+   wire        s_clk;
+   wire        s_maclk;
    
 
    /*******************************************************************************
@@ -47,51 +47,54 @@ module CPU_CS_ACAL_17(
    *******************************************************************************/
 
    /*** Outputs from Chip 30H  ***/
-   assign s_LUA[12] = q_lua_12_7[0];
-   assign s_LUA[11] = q_lua_12_7[1];
-   assign s_LUA[10] = q_lua_12_7[2];
-   //assign s_LUA[9:0] =  (outputs from Chip 31F)
+   assign s_lua[12] = s_q_chip30h_7_0[0];   
+   assign s_lua[11] = s_q_chip30h_7_0[1];
+   assign s_lua[10] = s_q_chip30h_7_0[2];
+
+   //assign s_lua[9:0] =  (outputs from Chip 31F)
    
+   // Not used signals
+   // s_q_chip30h_7_0[3];
+   // s_q_chip30h_7_0[4];
+   // s_q_chip30h_7_0[7];
    
-   assign s_UUA[11] = q_lua_12_7[5];
-   assign s_UUA[10] = q_lua_12_7[6];   
-   assign s_UUA[9:0] = s_LUA12 ? s_UUA_chip_32g[9:0] : s_UUA_chip_31g[9:0];
+   assign s_uua[11] = s_q_chip30h_7_0[5];
+   assign s_uua[10] = s_q_chip30h_7_0[6];   
+
+   assign s_uua[9:0] = s_lua12 ? s_uua_chip_32g_9_0[9:0] : s_uua_chip_31g_9_0[9:0];
 
    /*** Outputs from Chip 30H  ***/
 
    /* Input data for chip 30H */
-   assign s_D_chip30h[0] = s_CSA_12_0[12];
-   assign s_D_chip30h[1] = s_CSA_12_0[11];
-   assign s_D_chip30h[2] = s_CSA_12_0[10];
-   assign s_D_chip30h[3] = 0;
-   assign s_D_chip30h[4] = 0;
-   assign s_D_chip30h[5] = s_CSA_12_0[11] | s_LUA12_n;
-   assign s_D_chip30h[6] = s_CSA_12_0[10] | s_LUA12_n;
-   assign s_D_chip30h[7] = 0;
+   assign s_d_chip30h_7_0[0] = s_csa_12_0[12];
+   assign s_d_chip30h_7_0[1] = s_csa_12_0[11];
+   assign s_d_chip30h_7_0[2] = s_csa_12_0[10];
+   assign s_d_chip30h_7_0[3] = 0; // not used
+   assign s_d_chip30h_7_0[4] = 0; // not used
+   assign s_d_chip30h_7_0[5] = s_csa_12_0[11] | s_lua12_n;
+   assign s_d_chip30h_7_0[6] = s_csa_12_0[10] | s_lua12_n;
+   assign s_d_chip30h_7_0[7] = 0;
 
-   assign s_LUA12            = q_lua_12_7[0];
-   assign s_LUA12_n          = ~s_LUA12; // negated
+
+   assign s_lua12   =  s_lua[12];
+   assign s_lua12_n = ~s_lua12; // negated
+
+   
 
    /*******************************************************************************
    ** Here all input connections are defined                                     **
    *******************************************************************************/
-   assign s_CSCA_9_0[9:0]   = CSCA_9_0;
-   assign s_CSA_12_0[12:0]  = CSA_12_0;
-   assign s_PD1             = PD1;
-   assign s_MACLK           = MACLK;
-   assign s_CLK             = CLK;
+   assign s_csca_9_0[9:0]   = CSCA_9_0;
+   assign s_csa_12_0[12:0]  = CSA_12_0;
+   assign s_pd1             = PD1;
+   assign s_maclk           = MACLK;
+   assign s_clk             = CLK;
 
    /*******************************************************************************
    ** Here all output connections are defined                                    **
    *******************************************************************************/
-   assign LUA_12_0 = s_LUA[12:0];
-   assign UUA_11_0 = s_UUA[11:0];
-
-   /*******************************************************************************
-   ** Here all normal components are defined                                     **
-   *******************************************************************************/
-
-
+   assign LUA_12_0 = s_lua[12:0];
+   assign UUA_11_0 = s_uua[11:0];
 
    /*******************************************************************************
    ** Here all sub-circuits are defined                                          **
@@ -99,24 +102,24 @@ module CPU_CS_ACAL_17(
    
 
 
-   TTL_74373   CHIP_30H (.C(s_MACLK), 
-                         .D(s_D_chip30h[7:0]),
-                         .OC_n(s_PD1),
-                         .Q(q_lua_12_7[7:0]));
+   TTL_74373   CHIP_30H (.C(s_maclk), 
+                         .D(s_d_chip30h_7_0[7:0]),
+                         .OC_n(s_pd1),
+                         .Q(s_q_chip30h_7_0[7:0]));
 
-   AM29841   CHIP_31F (.D(s_CSA_12_0[9:0]),
-                       .LE(s_MACLK),  
-                       .OE_n(s_PD1),
-                       .Y(s_LUA[9:0]));
+   AM29841   CHIP_31F (.D(s_csa_12_0[9:0]),
+                       .LE(s_maclk),  
+                       .OE_n(s_pd1),
+                       .Y(s_lua[9:0]));
 
-   AM29841   CHIP_32G (.D(s_CSA_12_0[9:0]),
-                       .LE(s_MACLK),
-                       .OE_n(s_LUA12_n),
-                       .Y(s_UUA_chip_32g[9:0]));
+   AM29841   CHIP_32G (.D(s_csa_12_0[9:0]),
+                       .LE(s_maclk),
+                       .OE_n(s_lua12_n),
+                       .Y(s_uua_chip_32g_9_0[9:0]));
 
-   AM29841   CHIP_31G (.D(s_CSCA_9_0[9:0]),
-                       .LE(s_CLK), 
-                       .OE_n(s_LUA12),
-                       .Y(s_UUA_chip_31g[9:0]));
+   AM29841   CHIP_31G (.D(s_csca_9_0[9:0]),
+                       .LE(s_clk), 
+                       .OE_n(s_lua12),
+                       .Y(s_uua_chip_31g_9_0[9:0]));
 
 endmodule
