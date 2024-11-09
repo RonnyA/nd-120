dd2ad08ce6d22a8c8d2b835080c7a6740f0bba98
Ronny Hansen <ronnyha@live.no>
2024-11-09 18:03:58 +0100

    internal signal names refactored with s_ names matching signals

--- Verilog/DECODE-GateArray/DGA/circuit/F924.v
+++ Verilog/DECODE-GateArray/DGA/circuit/F924.v
@@ -1,126 +1,117 @@
-/******************************************************************************
- ** Logisim-evolution goes FPGA automatic generated Verilog code             **
- ** https://github.com/logisim-evolution/                                    **
- **                                                                          **
- ** Component : F924                                                         **
- **                                                                          **
- *****************************************************************************/
+/**************************************************************************
+** ND120 DGA (Decode Gate Array)                                         **
+** DECODE/DGA                                                            **
+**                                                                       **
+** NEC F924 - 4-BIT D-TYPE FLIP-FLOP                                     **
+**                                                                       **
+** Last reviewed: 9-NOV-2024                                             **
+** Ronny Hansen                                                          **
+***************************************************************************/
 
-module F924( C_H05,
-             D0_H01,
-             D1_H02,
-             D2_H03,
-             D3_H04,
-             N01_Q0,
-             N02_Q1,
-             N03_Q2,
-             N04_Q3,
-             N05_Q0B,
-             N06_Q1B,
-             N07_Q2B,
-             N08_Q3B );
+module F924 (
+    // Clock
+    input C_H05,
 
-   /*******************************************************************************
-   ** The inputs are defined here                                                **
-   *******************************************************************************/
-   input C_H05;
-   input D0_H01;
-   input D1_H02;
-   input D2_H03;
-   input D3_H04;
+    // Data inputs
+    input D0_H01,
+    input D1_H02,
+    input D2_H03,
+    input D3_H04,
 
-   /*******************************************************************************
-   ** The outputs are defined here                                               **
-   *******************************************************************************/
-   output N01_Q0;
-   output N02_Q1;
-   output N03_Q2;
-   output N04_Q3;
-   output N05_Q0B;
-   output N06_Q1B;
-   output N07_Q2B;
-   output N08_Q3B;
+    // Normal outputs
+    output N01_Q0,
+    output N02_Q1,
+    output N03_Q2,
+    output N04_Q3,
 
-   /*******************************************************************************
-   ** The wires are defined here                                                 **
-   *******************************************************************************/
-   wire s_logisimNet0;
-   wire s_logisimNet1;
-   wire s_logisimNet10;
-   wire s_logisimNet11;
-   wire s_logisimNet12;
-   wire s_logisimNet2;
-   wire s_logisimNet3;
-   wire s_logisimNet4;
-   wire s_logisimNet5;
-   wire s_logisimNet6;
-   wire s_logisimNet7;
-   wire s_logisimNet8;
-   wire s_logisimNet9;
+    // Negated outputs
+    output N05_Q0B,
+    output N06_Q1B,
+    output N07_Q2B,
+    output N08_Q3B
 
-   /*******************************************************************************
-   ** The module functionality is described here                                 **
-   *******************************************************************************/
+);
 
-   /*******************************************************************************
-   ** Here all input connections are defined                                     **
-   *******************************************************************************/
-   assign s_logisimNet1  = D0_H01;
-   assign s_logisimNet10 = C_H05;
-   assign s_logisimNet2  = D1_H02;
-   assign s_logisimNet3  = D2_H03;
-   assign s_logisimNet4  = D3_H04;
+  wire s_clock;
+  wire s_d0;
+  wire s_d1;
+  wire s_d2;
+  wire s_d3;
+  wire s_q0_n;
+  wire s_q0;
+  wire s_q1_n;
+  wire s_q1;
+  wire s_q2_n;
+  wire s_q2;
+  wire s_q3_n;
+  wire s_q3;
 
-   /*******************************************************************************
-   ** Here all output connections are defined                                    **
-   *******************************************************************************/
-   assign N01_Q0  = s_logisimNet6;
-   assign N02_Q1  = s_logisimNet7;
-   assign N03_Q2  = s_logisimNet8;
-   assign N04_Q3  = s_logisimNet9;
-   assign N05_Q0B = s_logisimNet5;
-   assign N06_Q1B = s_logisimNet12;
-   assign N07_Q2B = s_logisimNet0;
-   assign N08_Q3B = s_logisimNet11;
+  // Assign inputs
+  assign s_clock = C_H05;
 
-   /*******************************************************************************
-   ** Here all normal components are defined                                     **
-   *******************************************************************************/
-   D_FLIPFLOP #(.invertClockEnable(0))
-      MEMORY_1 (.clock(s_logisimNet10),
-                .d(s_logisimNet1),
-                .preset(1'b0),
-                .q(s_logisimNet6),
-                .qBar(s_logisimNet5),
-                .reset(1'b0),
-                .tick(1'b1));
+  assign s_d0 = D0_H01;
+  assign s_d1 = D1_H02;
+  assign s_d2 = D2_H03;
+  assign s_d3 = D3_H04;
 
-   D_FLIPFLOP #(.invertClockEnable(0))
-      MEMORY_2 (.clock(s_logisimNet10),
-                .d(s_logisimNet2),
-                .preset(1'b0),
-                .q(s_logisimNet7),
-                .qBar(s_logisimNet12),
-                .reset(1'b0),
-                .tick(1'b1));
+  // Assign outputs
+  assign N01_Q0 = s_q0;
+  assign N02_Q1 = s_q1;
+  assign N03_Q2 = s_q2;
+  assign N04_Q3 = s_q3;
+  assign N05_Q0B = s_q0_n;
+  assign N06_Q1B = s_q1_n;
+  assign N07_Q2B = s_q2_n;
+  assign N08_Q3B = s_q3_n;
 
-   D_FLIPFLOP #(.invertClockEnable(0))
-      MEMORY_3 (.clock(s_logisimNet10),
-                .d(s_logisimNet3),
-                .preset(1'b0),
-                .q(s_logisimNet8),
-                .qBar(s_logisimNet0),
-                .reset(1'b0),
-                .tick(1'b1));
+  // Implementation
+  D_FLIPFLOP #(
+      .invertClockEnable(0)
+  ) MEMORY_1 (
+      .clock(s_clock),
+      .d(s_d0),
+      .preset(1'b0),
+      .q(s_q0),
+      .qBar(s_q0_n),
+      .reset(1'b0),
+      .tick(1'b1)
+  );
 
-   D_FLIPFLOP #(.invertClockEnable(0))
-      MEMORY_4 (.clock(s_logisimNet10),
-                .d(s_logisimNet4),
-                .preset(1'b0),
-                .q(s_logisimNet9),
-                .qBar(s_logisimNet11),
-                .reset(1'b0),
-                .tick(1'b1));
+  D_FLIPFLOP #(
+      .invertClockEnable(0)
+  ) MEMORY_2 (
+      .clock(s_clock),
+      .d(s_d1),
+      .preset(1'b0),
+      .q(s_q1),
+      .qBar(s_q1_n),
+      .reset(1'b0),
+      .tick(1'b1)
+  );
+
+  D_FLIPFLOP #(
+      .invertClockEnable(0)
+  ) MEMORY_3 (
+      .clock(s_clock),
+      .d(s_d2),
+      .preset(1'b0),
+      .q(s_q2),
+      .qBar(s_q2_n),
+      .reset(1'b0),
+      .tick(1'b1)
+  );
+
+  D_FLIPFLOP #(
+      .invertClockEnable(0)
+  ) MEMORY_4 (
+      .clock(s_clock),
+      .d(s_d3),
+      .preset(1'b0),
+      .q(s_q3),
+      .qBar(s_q3_n),
+      .reset(1'b0),
+      .tick(1'b1)
+  );
 
 
 endmodule
