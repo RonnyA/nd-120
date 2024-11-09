6c6b80f19f7982ec6f502ae619b4477e54284886
Ronny Hansen <ronnyha@live.no>
2024-11-09 19:36:39 +0100

    internal signal names refactored with s_ names matching signals

--- Verilog/DELILAH-CPU/CGA_IDBCTL/circuit/CGA_IDBCTL_PGSREG.v
+++ Verilog/DELILAH-CPU/CGA_IDBCTL/circuit/CGA_IDBCTL_PGSREG.v
@@ -1,199 +1,192 @@
-/******************************************************************************
- ** Logisim-evolution goes FPGA automatic generated Verilog code             **
- ** https://github.com/logisim-evolution/                                    **
- **                                                                          **
- ** Component : CGA_IDBCTL_PGSREG                                            **
- **                                                                          **
- *****************************************************************************/
+/**************************************************************************
+** CPU GATE ARRAY - CGA - DELILAH                                        **
+**                                                                       **
+** CGA/IDBCTL/PGSREG - IDB Control Logic                                 **
+**                                                                       **
+** PDF page 98 of 108                                                    **
+**                                                                       **
+** Last reviewed: 9-NOV-2024                                             **
+** Ronny Hansen                                                          **
+***************************************************************************/
 
-module CGA_IDBCTL_PGSREG( FETCHN,
-                          LA_21_10,
-                          MCLK,
-                          PGS_11_0,
-                          PGS_15_14,
-                          PVIOL,
-                          VACCN );
 
-   /*******************************************************************************
-   ** The inputs are defined here                                                **
-   *******************************************************************************/
-   input        FETCHN;
-   input [11:0] LA_21_10;
-   input        MCLK;
-   input        PVIOL;
-   input        VACCN;
 
-   /*******************************************************************************
-   ** The outputs are defined here                                               **
-   *******************************************************************************/
-   output [11:0] PGS_11_0;
-   output [1:0]  PGS_15_14;
+module CGA_IDBCTL_PGSREG (
+    input FETCHN,
+    input [11:0] LA_21_10,
+    input MCLK,
+    input PVIOL,
+    input VACCN,
 
-   /*******************************************************************************
+    output [11:0] PGS_11_0,
+    output [ 1:0] PGS_15_14
+);
+
+  /*******************************************************************************
    ** The wires are defined here                                                 **
    *******************************************************************************/
-   wire [11:0] s_logisimBus30;
-   wire [1:0]  s_logisimBus32;
-   wire [11:0] s_logisimBus34;
-   wire        s_logisimNet0;
-   wire        s_logisimNet1;
-   wire        s_logisimNet10;
-   wire        s_logisimNet11;
-   wire        s_logisimNet12;
-   wire        s_logisimNet13;
-   wire        s_logisimNet14;
-   wire        s_logisimNet15;
-   wire        s_logisimNet16;
-   wire        s_logisimNet17;
-   wire        s_logisimNet18;
-   wire        s_logisimNet19;
-   wire        s_logisimNet2;
-   wire        s_logisimNet20;
-   wire        s_logisimNet21;
-   wire        s_logisimNet22;
-   wire        s_logisimNet23;
-   wire        s_logisimNet24;
-   wire        s_logisimNet25;
-   wire        s_logisimNet26;
-   wire        s_logisimNet27;
-   wire        s_logisimNet28;
-   wire        s_logisimNet29;
-   wire        s_logisimNet3;
-   wire        s_logisimNet31;
-   wire        s_logisimNet33;
-   wire        s_logisimNet4;
-   wire        s_logisimNet5;
-   wire        s_logisimNet6;
-   wire        s_logisimNet7;
-   wire        s_logisimNet8;
-   wire        s_logisimNet9;
+  wire        s_fetch_n;
+  wire        s_mclk;
+  wire        s_pgs15_dq;
+  wire        s_pviol;
+  wire        s_vacc_n;
+  wire        s_vacc;
+  wire [ 1:0] s_pga_15_14_out;
+  wire [11:0] s_la_21_10;
+  wire [11:0] s_pgs_11_0_out;
 
-   /*******************************************************************************
-   ** The module functionality is described here                                 **
-   *******************************************************************************/
-
-   /*******************************************************************************
+  /*******************************************************************************
    ** Here all input connections are defined                                     **
    *******************************************************************************/
-   assign s_logisimBus34[11:0] = LA_21_10;
-   assign s_logisimNet0        = MCLK;
-   assign s_logisimNet14       = FETCHN;
-   assign s_logisimNet25       = PVIOL;
-   assign s_logisimNet33       = VACCN;
+  assign s_la_21_10[11:0] = LA_21_10;
+  assign s_mclk           = MCLK;
+  assign s_fetch_n        = FETCHN;
+  assign s_pviol          = PVIOL;
+  assign s_vacc_n         = VACCN;
 
-   /*******************************************************************************
+  /*******************************************************************************
    ** Here all output connections are defined                                    **
    *******************************************************************************/
-   assign PGS_11_0  = s_logisimBus30[11:0];
-   assign PGS_15_14 = s_logisimBus32[1:0];
+  assign PGS_11_0         = s_pgs_11_0_out[11:0];
+  assign PGS_15_14        = s_pga_15_14_out[1:0];
 
-   /*******************************************************************************
+  /*******************************************************************************
    ** Here all in-lined components are defined                                   **
    *******************************************************************************/
 
-   // NOT Gate
-   assign s_logisimNet5 = ~s_logisimNet33;
+  // NOT Gate
+  assign s_vacc    = ~s_vacc_n;
 
-   /*******************************************************************************
+  /*******************************************************************************
    ** Here all sub-circuits are defined                                          **
    *******************************************************************************/
 
-   SCAN_FF   PGS9 (.CLK(s_logisimNet0),
-                   .D(s_logisimBus30[9]),
-                   .Q(s_logisimBus30[9]),
-                   .QN(),
-                   .TE(s_logisimNet5),
-                   .TI(s_logisimBus34[9]));
 
-   SCAN_FF   PGS8 (.CLK(s_logisimNet0),
-                   .D(s_logisimBus30[8]),
-                   .Q(s_logisimBus30[8]),
-                   .QN(),
-                   .TE(s_logisimNet5),
-                   .TI(s_logisimBus34[8]));
+  SCAN_FF PGS15 (
+      .CLK(s_mclk),
+      .D  (s_pgs15_dq),
+      .Q  (s_pgs15_dq),
+      .QN (s_pga_15_14_out[1]),
+      .TE (s_vacc),
+      .TI (s_fetch_n)
+  );
 
-   SCAN_FF   PGS7 (.CLK(s_logisimNet0),
-                   .D(s_logisimBus30[7]),
-                   .Q(s_logisimBus30[7]),
-                   .QN(),
-                   .TE(s_logisimNet5),
-                   .TI(s_logisimBus34[7]));
+  SCAN_FF PGS14 (
+      .CLK(s_mclk),
+      .D  (s_pga_15_14_out[0]),
+      .Q  (s_pga_15_14_out[0]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_pviol)
+  );
 
-   SCAN_FF   PGS15 (.CLK(s_logisimNet0),
-                    .D(s_logisimNet27),
-                    .Q(s_logisimNet27),
-                    .QN(s_logisimBus32[1]),
-                    .TE(s_logisimNet5),
-                    .TI(s_logisimNet14));
+  SCAN_FF PGS11 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[11]),
+      .Q  (s_pgs_11_0_out[11]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[11])
+  );
 
-   SCAN_FF   PGS14 (.CLK(s_logisimNet0),
-                    .D(s_logisimBus32[0]),
-                    .Q(s_logisimBus32[0]),
-                    .QN(),
-                    .TE(s_logisimNet5),
-                    .TI(s_logisimNet25));
+  SCAN_FF PGS10 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[10]),
+      .Q  (s_pgs_11_0_out[10]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[10])
+  );
 
-   SCAN_FF   PGS11 (.CLK(s_logisimNet0),
-                    .D(s_logisimBus30[11]),
-                    .Q(s_logisimBus30[11]),
-                    .QN(),
-                    .TE(s_logisimNet5),
-                    .TI(s_logisimBus34[11]));
+  SCAN_FF PGS9 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[9]),
+      .Q  (s_pgs_11_0_out[9]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[9])
+  );
 
-   SCAN_FF   PGS10 (.CLK(s_logisimNet0),
-                    .D(s_logisimBus30[10]),
-                    .Q(s_logisimBus30[10]),
-                    .QN(),
-                    .TE(s_logisimNet5),
-                    .TI(s_logisimBus34[10]));
+  SCAN_FF PGS8 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[8]),
+      .Q  (s_pgs_11_0_out[8]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[8])
+  );
 
-   SCAN_FF   PGS3 (.CLK(s_logisimNet0),
-                   .D(s_logisimBus30[3]),
-                   .Q(s_logisimBus30[3]),
-                   .QN(),
-                   .TE(s_logisimNet5),
-                   .TI(s_logisimBus34[3]));
+  SCAN_FF PGS7 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[7]),
+      .Q  (s_pgs_11_0_out[7]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[7])
+  );
 
-   SCAN_FF   PGS2 (.CLK(s_logisimNet0),
-                   .D(s_logisimBus30[2]),
-                   .Q(s_logisimBus30[2]),
-                   .QN(),
-                   .TE(s_logisimNet5),
-                   .TI(s_logisimBus34[2]));
 
-   SCAN_FF   PGS1 (.CLK(s_logisimNet0),
-                   .D(s_logisimBus30[1]),
-                   .Q(s_logisimBus30[1]),
-                   .QN(),
-                   .TE(s_logisimNet5),
-                   .TI(s_logisimBus34[1]));
+  SCAN_FF PGS3 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[3]),
+      .Q  (s_pgs_11_0_out[3]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[3])
+  );
 
-   SCAN_FF   PGS0 (.CLK(s_logisimNet0),
-                   .D(s_logisimBus30[0]),
-                   .Q(s_logisimBus30[0]),
-                   .QN(),
-                   .TE(s_logisimNet5),
-                   .TI(s_logisimBus34[0]));
+  SCAN_FF PGS2 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[2]),
+      .Q  (s_pgs_11_0_out[2]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[2])
+  );
 
-   SCAN_FF   PGS6 (.CLK(s_logisimNet0),
-                   .D(s_logisimBus30[6]),
-                   .Q(s_logisimBus30[6]),
-                   .QN(),
-                   .TE(s_logisimNet5),
-                   .TI(s_logisimBus34[6]));
+  SCAN_FF PGS1 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[1]),
+      .Q  (s_pgs_11_0_out[1]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[1])
+  );
 
-   SCAN_FF   PGS5 (.CLK(s_logisimNet0),
-                   .D(s_logisimBus30[5]),
-                   .Q(s_logisimBus30[5]),
-                   .QN(),
-                   .TE(s_logisimNet5),
-                   .TI(s_logisimBus34[5]));
+  SCAN_FF PGS0 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[0]),
+      .Q  (s_pgs_11_0_out[0]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[0])
+  );
 
-   SCAN_FF   PGS4 (.CLK(s_logisimNet0),
-                   .D(s_logisimBus30[4]),
-                   .Q(s_logisimBus30[4]),
-                   .QN(),
-                   .TE(s_logisimNet5),
-                   .TI(s_logisimBus34[4]));
+  SCAN_FF PGS6 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[6]),
+      .Q  (s_pgs_11_0_out[6]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[6])
+  );
+
+  SCAN_FF PGS5 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[5]),
+      .Q  (s_pgs_11_0_out[5]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[5])
+  );
+
+  SCAN_FF PGS4 (
+      .CLK(s_mclk),
+      .D  (s_pgs_11_0_out[4]),
+      .Q  (s_pgs_11_0_out[4]),
+      .QN (),
+      .TE (s_vacc),
+      .TI (s_la_21_10[4])
+  );
 
 endmodule
--- Verilog/DELILAH-CPU/CGA_IDBCTL/circuit/CGA_IDBCTL_SEL6.v
+++ Verilog/DELILAH-CPU/CGA_IDBCTL/circuit/CGA_IDBCTL_SEL6.v
@@ -1,121 +1,97 @@
-/******************************************************************************
- ** Logisim-evolution goes FPGA automatic generated Verilog code             **
- ** https://github.com/logisim-evolution/                                    **
- **                                                                          **
- ** Component : CGA_IDBCTL_SEL6                                              **
- **                                                                          **
- *****************************************************************************/
+/**************************************************************************
+** CPU GATE ARRAY - CGA - DELILAH                                        **
+**                                                                       **
+** CGA/IDBCTL/SEL6 - IDB Control Logic                                   **
+**                                                                       **
+** PDF page 99 of 108                                                    **
+**                                                                       **
+** Last reviewed: 9-NOV-2024                                             **
+** Ronny Hansen                                                          **
+***************************************************************************/
 
-module CGA_IDBCTL_SEL6( D,
-                        D0,
-                        E_PINS,
-                        M,
-                        PCR,
-                        PGS,
-                        S,
-                        V );
 
-   /*******************************************************************************
-   ** The inputs are defined here                                                **
-   *******************************************************************************/
-   input       D;
-   input [5:0] E_PINS;
-   input       M;
-   input       PCR;
-   input       PGS;
-   input       S;
-   input       V;
+module CGA_IDBCTL_SEL6 (
+    input D,
 
-   /*******************************************************************************
-   ** The outputs are defined here                                               **
-   *******************************************************************************/
-   output D0;
+    //ED=5, EM=4, EV=3, ES=2, EPCR=1, EPGS=0
+    input [5:0] E_PINS,
 
-   /*******************************************************************************
+    input M,
+    input PCR,
+    input PGS,
+    input S,
+    input V,
+
+    output D0
+);
+
+
+
+  /*******************************************************************************
    ** The wires are defined here                                                 **
    *******************************************************************************/
-   wire [5:0] s_logisimBus19;
-   wire       s_logisimNet0;
-   wire       s_logisimNet1;
-   wire       s_logisimNet10;
-   wire       s_logisimNet11;
-   wire       s_logisimNet12;
-   wire       s_logisimNet13;
-   wire       s_logisimNet14;
-   wire       s_logisimNet15;
-   wire       s_logisimNet16;
-   wire       s_logisimNet17;
-   wire       s_logisimNet18;
-   wire       s_logisimNet2;
-   wire       s_logisimNet3;
-   wire       s_logisimNet4;
-   wire       s_logisimNet5;
-   wire       s_logisimNet6;
-   wire       s_logisimNet7;
-   wire       s_logisimNet8;
-   wire       s_logisimNet9;
 
-   /*******************************************************************************
-   ** The module functionality is described here                                 **
-   *******************************************************************************/
+  wire s_out_v;
+  wire s_out_m;
+  wire s_out_pcr;
+  wire s_out_d;
+  wire s_out_s;
+  wire s_out_pgs;
 
-   /*******************************************************************************
+  wire s_out_d0;
+
+  wire s_d;
+  wire s_m;
+  wire s_v;
+  wire s_s;
+  wire s_pcr;
+  wire s_pgs;
+
+  wire s_ed;
+  wire s_em;
+  wire s_ev;
+  wire s_es;
+  wire s_epcr;
+  wire s_epgs;
+
+  /*******************************************************************************
    ** Here all input connections are defined                                     **
    *******************************************************************************/
-   assign s_logisimBus19[5:0] = E_PINS;
-   assign s_logisimNet2       = D;
-   assign s_logisimNet3       = M;
-   assign s_logisimNet4       = V;
-   assign s_logisimNet5       = S;
-   assign s_logisimNet6       = PCR;
-   assign s_logisimNet7       = PGS;
 
-   /*******************************************************************************
+  assign s_d       = D;
+  assign s_m       = M;
+  assign s_v       = V;
+  assign s_s       = S;
+  assign s_pcr     = PCR;
+  assign s_pgs     = PGS;
+
+  assign s_ed      = E_PINS[5];
+  assign s_em      = E_PINS[4];
+  assign s_ev      = E_PINS[3];
+  assign s_es      = E_PINS[2];
+  assign s_epcr    = E_PINS[1];
+  assign s_epgs    = E_PINS[0];
+
+
+  /*******************************************************************************
    ** Here all output connections are defined                                    **
    *******************************************************************************/
-   assign D0 = s_logisimNet18;
+  assign D0        = s_out_d0;
 
-   /*******************************************************************************
+  /*******************************************************************************
    ** Here all normal components are defined                                     **
    *******************************************************************************/
-   NAND_GATE #(.BubblesMask(2'b00))
-      GATES_1 (.input1(s_logisimNet2),
-               .input2(s_logisimBus19[5]),
-               .result(s_logisimNet13));
 
-   NAND_GATE #(.BubblesMask(2'b00))
-      GATES_2 (.input1(s_logisimNet3),
-               .input2(s_logisimBus19[4]),
-               .result(s_logisimNet11));
+  /* Refactored the code to not use negated output on the AND chips, and removed the negation on the input on the 6 pin OR */
 
-   NAND_GATE #(.BubblesMask(2'b00))
-      GATES_3 (.input1(s_logisimNet4),
-               .input2(s_logisimBus19[3]),
-               .result(s_logisimNet1));
+  assign s_out_d   = (s_d & s_ed);
+  assign s_out_m   = (s_m & s_em);
+  assign s_out_v   = (s_v & s_ev);
+  assign s_out_s   = (s_s & s_es);
+  assign s_out_pcr = (s_pcr & s_epcr);
+  assign s_out_pgs = (s_pgs & s_epgs);
 
-   NAND_GATE #(.BubblesMask(2'b00))
-      GATES_4 (.input1(s_logisimNet5),
-               .input2(s_logisimBus19[2]),
-               .result(s_logisimNet15));
-
-   NAND_GATE #(.BubblesMask(2'b00))
-      GATES_5 (.input1(s_logisimNet6),
-               .input2(s_logisimBus19[1]),
-               .result(s_logisimNet12));
-
-   NAND_GATE #(.BubblesMask(2'b00))
-      GATES_6 (.input1(s_logisimNet7),
-               .input2(s_logisimBus19[0]),
-               .result(s_logisimNet17));
-
-   OR_GATE_6_INPUTS #(.BubblesMask({2'b11, 4'hF}))
-      GATES_7 (.input1(s_logisimNet13),
-               .input2(s_logisimNet11),
-               .input3(s_logisimNet1),
-               .input4(s_logisimNet15),
-               .input5(s_logisimNet12),
-               .input6(s_logisimNet17),
-               .result(s_logisimNet18));
+  assign s_out_d0  = s_epgs | s_out_m | s_out_v | s_out_s | s_out_pcr | s_out_pgs;
 
 
 endmodule
