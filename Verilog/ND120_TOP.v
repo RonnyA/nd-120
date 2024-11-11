9ea2ca7e0c4529c72f84f704dbda29cc5e291010
Ronny Hansen <ronnyha@live.no>
2024-11-11 08:35:54 +0100

    Renamed top file

--- Verilog/ND120_TOP.v
+++ Verilog/ND120_TOP.v
@@ -21,7 +21,7 @@
 //! @author Ronny Hansen
 //! TOP LEVEL FOR FPGA IMPLEMENTATION OF ND-3202D CPU BOARD
 
-module TOP (
+module ND120_TOP (
     input  wire       sysclk,  //! System Clock
     input  wire       btn1,    //! Button 1, mapped to S1 (not labeled) on the board
     input  wire       btn2,    //! Button 2, mapped to S2 (not labeled) on the board
@@ -72,8 +72,6 @@ module TOP (
   assign sys_rst_n = btn1;
   assign oc_select = 2'b11;  // 11= Choose clock input = XTAL1 (full speed)
 
-
-
   // Assign som lights to the LED's (Inverted because the nano is led's are active low)
 
   assign led[1:0] = ~s_cpu_led[1:0];  //  0=RED,1=GREEN
@@ -86,26 +84,35 @@ module TOP (
   ND3202D CPU_BOARD (
       .sysclk(sysclk),
       .sys_rst_n(sys_rst_n),
+      .CLOCK_1(clk1),  // XTAL1 = 39.3216MHZ
+      .CLOCK_2(clk1),  // XTAL2 = 35 MHZ (for slow operations?)
+
+      // Signal from C-PLUG
+      .LOAD_n(s_high),
+      .BREQ_n(s_high),
+      .CONTINUE_n(s_high),
+      .STOP_n(s_high),
+
       .BINT10_n(s_high),
       .BINT11_n(s_high),
       .BINT12_n(s_high),
       .BINT13_n(s_high),
       .BINT15_n(s_high),
-      .BREQ_n(s_high),
-      .CLOCK_1(clk1),
-      .CLOCK_2(clk1),
-      .CONSOLE_n(s_high),
-      .CONTINUE_n(s_high),
-      .EAUTO_n(s_high),
-      .LOAD_n(s_high),
-      .LOCK_n(s_high),
-      .OC_1_0(oc_select),
+
+      .POWSENSE_n(s_high),
+
+      // Signals from A-PLUG
       .OSCCL_n(s_high),
-      .RXD(uartRx),
-      .STOP_n(s_high),
-      .SW1_CONSOLE(s_high),
-      .SWMCL_n(s_high),
+      .OC_1_0(oc_select),
       .XTR(s_low),
+      .LOCK_n(s_high),
+      .CONSOLE_n(s_high),
+      .SWMCL_n(s_high),
+      .EAUTO_n(s_high),
+      .RXD(uartRx),
+
+      /* Configuration switches */
+      .SW1_CONSOLE(s_high),
       .SEL_TESTMUX(s_SEL_TESTMUX),
       .BAUD_RATE_SWITCH(s_baud_rate_switch),  // 5 bits
 
