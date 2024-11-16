8236cf604e2b4da484da58171b2c419f9c25beec
Ronny Hansen <ronnyha@live.no>
2024-11-16 12:13:42 +0100

    Added comments

--- Verilog/CPU-BOARD-3202/circuit/CPU_MMU_CSR_26.v
+++ Verilog/CPU-BOARD-3202/circuit/CPU_MMU_CSR_26.v
@@ -7,30 +7,31 @@
 ** Last reviewed: 19-FEB-2024                                            **
 ** Ronny Hansen                                                          **
 ***************************************************************************/
-module CPU_MMU_CSR_26(
-   input STP,
-   input EMPID_n,
-   input EDO_n,
-   input LCS_n,
-   input PD2, // OE1_1G_n
+module CPU_MMU_CSR_26 (
+    input STP,
+    input EMPID_n,
+    input EDO_n,
+    input LCS_n,  //! Load Control Store
+    input PD2,  // OE1_1G_n  //! P Disable2 - Always 0
 
-   input CUP,
-   input CON,
-   input ECSR_n, // OE2_2G_n
+    input CUP,    //! Cache Updated (CUP) goes to IDB0 when ECSR_n is low 
+    input CON,    //! Cache ON (CON) goes to IDB1 when ECSR_n is low. CON_n goes to IDB2
+    input ECSR_n, // OE2_2G_n //! Enable Cache Status Reg
 
-   output       BSTP,
-   output       BEMPID_n,
-   output       BEDO_n,
-   output       BLCS_n,
+    // IF PD2 goes active to 1, then all B-signals goes to high-impediance aka 0)
+    output BSTP,      //! Buffered STP 
+    output BEMPID_n,  //! Buffered EMPID_n
+    output BEDO_n,    //! Buffered EDO_n
+    output BLCS_n,    //! Buffered LCS_n
 
-   output [3:0] IDB_3_0
+    output [3:0] IDB_3_0  // Bit 0=CUP, Bit 1=CON, Bit 2= CON_n, Bit 3=1 (FIN-Cache Clear finished. Not currently enabled)
 );
 
 
 
-// This code replaces one 74244 (CHIP 27H)
-// 74LS244 (NONE negated outputs)
-// Octal Buffers and Line Drivers With 3-State Outputs
+  // This code replaces one 74244 (CHIP 27H)
+  // 74LS244 (NONE negated outputs)
+  // Octal Buffers and Line Drivers With 3-State Outputs
 
 
   assign BSTP = PD2 ? 1'b0 : STP;
