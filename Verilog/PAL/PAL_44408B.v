7fe89d7363c0540020560dc84dc881c5bb9216ff
Ronny Hansen <ronnyha@live.no>
2024-11-17 22:40:30 +0100

    Added more comments

--- Verilog/PAL/PAL_44408B.v
+++ Verilog/PAL/PAL_44408B.v
@@ -10,8 +10,8 @@
 
 
 /*
-; TO FIX VIRTUAL EXAMINE BUG IN CGA 
-; + MISC. OTHER BUGS. 
+; TO FIX VIRTUAL EXAMINE BUG IN CGA
+; + MISC. OTHER BUGS.
 
 PAL 44608A (VXFIX) replaces this PAL. The new PAL 44608A is an PAL16R6 that has a pin RT_n output. Use RT_n signal from DGA until we find out what the 44608A does with this signal.
 */
@@ -20,25 +20,25 @@ module PAL_44408B (
     input CK,   //! Clock signal
     input OE_n, //! OUTPUT ENABLE (active-low) for Q0-Q3
 
-    input C4,     //! I0 - CSCOMM4
-    input C3,     //! I1 - CSCOMM3
-    input C2,     //! I2 - CSCOMM2 
-    input C1,     //! I3 - CSCOMM1 
-    input C0,     //! I4 - CSCOMM0 
-    input M1,     //! I5 - CSMIS1
-    input M0,     //! I6 - CSMIS0  
+    input C4,     //! I0 - CSCOMM4 - CSBIT# 36 - Command bit 4
+    input C3,     //! I1 - CSCOMM3 - CSBIT# 35 - Command bit 3
+    input C2,     //! I2 - CSCOMM2 - CSBIT# 34 - Command bit 2
+    input C1,     //! I3 - CSCOMM1 - CSBIT# 33 - Command bit 1
+    input C0,     //! I4 - CSCOMM0 - CSBIT# 32 - Command bit 0
+    input M1,     //! I5 - CSMIS1   - CSBIT #43 - ALU MIS1 SIGNAL (also used for sub-commands for CSCOMM)
+    input M0,     //! I6 - CSMIS0   - CSBIT #42 - ALU MIS0 SIGNAL (also used for sub-commands for CSCOMM)
                   //! I7 - (not connected)
-    input LCS_n,
+    input LCS_n,  //! B0 - Load Control Store (negated)
     // B0_n - (not connected)
     input IDB2,   //! B1_n - IDB2
 
     // These signals may have wrong Q pin description, as this is taken from the description of the PAL 444608 (VXFIX) which is a PAL16R6
 
     // Q0_n - (not connected)
-    output LDEXM_n,  //! Q1_n
-    output VEX,      //! Q2_n
-    output OPCLCS,   //! Q3_n
-    output RWCS_n,   //! Q4_n
+    output LDEXM_n,  //! Q1_n - Command 21.3
+    output VEX,      //! Q2_n - ?
+    output OPCLCS,   //! Q3_n - Command 36.2 (LCS)
+    output RWCS_n,   //! Q4_n - Command 36.1 (RWCS)
 
     // Pin in 444608 (VXFIX) but not in 44408B
     output RT_n  //! Q5_n
@@ -91,6 +91,6 @@ DESCRIPTION
 
 ; 180587 M3202B
 ; 150687 RWCS SIGNAL TOO SLOW FROM NEC G.A. MUST BE DONE IN A PAL.
-; 010787 REGISTRATION NUMBER CHANGED FROM 44501B 
+; 010787 REGISTRATION NUMBER CHANGED FROM 44501B
 
 */
