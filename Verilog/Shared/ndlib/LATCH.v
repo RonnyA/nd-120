fe114089f4c2aa9e275ca650abf84cc518fd8116
Ronny Hansen <ronnyha@live.no>
2024-11-24 10:48:07 +0100

    Improved logic

--- Verilog/Shared/ndlib/LATCH.v
+++ Verilog/Shared/ndlib/LATCH.v
@@ -1,33 +1,36 @@
-/******************************************************************************
- **                                                                          **
- ** Component : LATCH                                                        **
- **                                                                          **
- *****************************************************************************/
+/**************************************************************************
+** ND120 Shared                                                          **
+**                                                                       **
+** Component LATCH                                                       **
+**                                                                       **
+** Last reviewed: 24-NOV-2024                                            **
+** Ronny Hansen                                                          **
+***************************************************************************/
 
-module LATCH(
-      input wire D,
-      input wire ENABLE,
-      output reg Q,
-      output reg QN );
+module LATCH (
+    input  wire D,
+    input  wire ENABLE,
+    output wire Q,
+    output wire QN
+);
+
+  reg regD;
+  assign Q = regD;  //Assign Q
+  assign QN = ~regD;  // Assign Q_n
+
+  // Initialize register to 0
+  initial begin
+    regD = 1'b0;
+  end
 
 
-  // Initialize Q and QN with opposite values
-    initial begin
-        Q = 1'b0;
-        QN = 1'b1;
+  // LATCH
+  always @(D or ENABLE) begin
+    if (ENABLE) begin
+      regD <= D;  // When ENABLE is high, Q takes the value of the input
     end
- 
-   // LATCH
-   always @* begin 
-      if (ENABLE) begin
-         Q = D; // When ENABLE is high, Q takes the value of the input
-      end
-      // When ENABLE is low, Q retains its value
-      
-      QN = ~Q; // Negate Q to get QN
-   end
-
-   
+    // When ENABLE is low, Q retains its value
+  end
 
 endmodule
 
