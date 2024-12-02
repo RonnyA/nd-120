7e01bb96cbf084700a35c0c2102c256c5e52ac77
Ronny Hansen <ronnyha@live.no>
2024-12-02 17:36:09 +0100

    Refactored and tested BusDriver

--- Verilog/DELILAH-CPU/CGA/circuit/BusDriver16.v
+++ Verilog/DELILAH-CPU/CGA/circuit/BusDriver16.v
@@ -3,48 +3,46 @@
 ** ND120 CPU, MM&M                                                       **
 ** CPU/PROC/CGA                                                          **
 **  BusDriver16                                                          **
-**                                                                       ** 
+**                                                                       **
 ** SHEET 4 of 8 (Page 5 in PDF)                                          **
-**                                                                       ** 
-** Last reviewed: 15-JAN-2024                                            **
+**                                                                       **
+** Last reviewed: 1-DEC-2024                                             **
 ** Ronny Hansen                                                          **
 ***************************************************************************/
 
 
 
-module BusDriver16(
-       input wire        EN,            // Enable = FALSE => A to IO, Enable=TRUE => IO to ZI
-       input wire        TN,            // Test enable
+module BusDriver16 (
+    input wire EN,  // Enable = FALSE => A to IO, Enable=TRUE => IO to A
+    input wire TN,  // Test enable
 
-       input wire[15:0]   A_15_0,        // Data inputA (Connect to internal FIDBO data bus))         
+    input  wire [15:0] A_15_0_IN,  // Data inputA (Connect to internal FIDBO data bus))
+    output wire [15:0] A_15_0_OUT, // A output  (Connect to internal XFIDBI data bus)
 
-       input wire[15:0]   IO_15_0_IN,    // IN and OUT to XFIDB data bus (Connect to EXTERNAL _XFIDB_ data bus)
-       output wire[15:0]  IO_15_0_OUT,   // 
+    input wire[15:0]   IO_15_0_IN,    // IN and OUT to XFIDB data bus (Connect to EXTERNAL _XFIDB_ data bus)
+    output wire [15:0] IO_15_0_OUT  //
+);
 
-       output wire[15:0]  ZI_15_0        // Z output  (Connect to internal XFIDBI data bus)
-   );
+  reg [15:0] IO_reg;  // Internal data register
+  reg [15:0] A_reg;  // Internal data register
 
-   reg [15:0] IO_reg; // Internal data register
-   reg [15:0] ZI_reg; // Internal data register
+  wire s_enable = ~EN;
 
+  //assign ZI_15_0 = TN ?  IO_15_0 : 16'b0; // Probably the correct implementation ? In test moode we should be isolated ?
+  //assign ZI_15_0 = TN ?  internalData : 16'b0; // Probably the correct implementation ? In test moode we should be isolated ?
 
-   //assign ZI_15_0 = TN ?  IO_15_0 : 16'b0; // Probably the correct implementation ? In test moode we should be isolated ?
-   //assign ZI_15_0 = TN ?  internalData : 16'b0; // Probably the correct implementation ? In test moode we should be isolated ?
+  always @(EN, TN, A_15_0_IN, IO_15_0_IN) begin
+    if (s_enable) begin
+      // Write A to IO
+      IO_reg <= A_15_0_IN;
+    end else begin
+      // Read IO to ZI
+      A_reg <= IO_15_0_IN;
+    end
+  end
 
-   always @(*) begin
-      if (EN == 1'b0) begin
-         // Write A to IO
-         IO_reg  = A_15_0;         
-      end
-      else begin
-         // Read IO to ZI 
-         ZI_reg = IO_15_0_IN;
-      end
-   end
-
-   // Bidirectional data operation
-   assign IO_15_0_OUT = TN ? ((EN == 1'b0) ? IO_reg : 16'b0) : 16'b0;     
-   assign ZI_15_0 = ZI_reg;
-
+  // Bidirectional data operation
+  assign IO_15_0_OUT = TN ? ((s_enable) ? IO_reg : 16'b0) : 16'b0;
+  assign A_15_0_OUT  = IO_15_0_IN; //A_reg; // A is always read from IO
 
 endmodule
