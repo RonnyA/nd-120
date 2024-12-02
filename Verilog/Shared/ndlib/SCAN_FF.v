63d42b779d12306f74916336e3d3830ebce0a50e
Ronny Hansen <ronnyha@live.no>
2024-12-02 18:52:19 +0100

    Refactored (removed AND/OR components and did logic direct)

--- Verilog/Shared/ndlib/SCAN_FF.v
+++ Verilog/Shared/ndlib/SCAN_FF.v
@@ -6,21 +6,21 @@
 ** Positive edge triggered D flip-flop with scan                         **
 ** Its a D flip-flop with a 2-input multiplexer on the D input           **
 **                                                                       **
-** When TE (test enable) input is negated,                               **
+** When TE (T enable) input is negated,                                  **
 ** the circuit behaves like an ordinary D flip-flop.                     **
 **                                                                       **
 ** When TE is asserted,                                                  **
-** it takes its data from TI (test input) instead of from D.             **
+** it takes its data from TI (T input) instead of from D.                **
 **                                                                       **
-** Last reviewed: 9-NOV-2024                                             **
+** Last reviewed: 1-DEC-2024                                             **
 ** Ronny Hansen                                                          **
 ***************************************************************************/
 
 module SCAN_FF (
     input CLK,  //! Clock (positive triggered)
     input D,    //! D input
-    input TE,   //! Test enable
-    input TI,   //! Test input
+    input TE,   //! T enable
+    input TI,   //! T Input
 
     output Q,  //! Q output
     output QN  //! Q_n output
@@ -29,23 +29,23 @@ module SCAN_FF (
   /*******************************************************************************
    ** The wires are defined here                                                 **
    *******************************************************************************/
-  wire s_te;
-  wire s_gates1_out;
-  wire s_gates2_out;
   wire s_clk;
-  wire s_gates3_out;
+  wire s_d_and_te_n;
+  wire s_d;
+  wire s_ff_d_input;
   wire s_q_out;
   wire s_qn_out;
-  wire s_d;
-  wire s_ti;
   wire s_te_n;
+  wire s_te;
+  wire s_ti_and_te;
+  wire s_ti;
 
   /*******************************************************************************
    ** Here all input connections are defined                                     **
    *******************************************************************************/
-  assign s_te = TE;
   assign s_clk = CLK;
   assign s_d = D;
+  assign s_te = TE;
   assign s_ti = TI;
 
   /*******************************************************************************
@@ -64,41 +64,33 @@ module SCAN_FF (
   /*******************************************************************************
    ** Here all normal components are defined                                     **
    *******************************************************************************/
-  AND_GATE #(
-      .BubblesMask(2'b00)
-  ) GATES_1 (
-      .input1(s_d),
-      .input2(s_te_n),
-      .result(s_gates1_out)
-  );
+  assign s_d_and_te_n = (s_d & s_te_n);
+  assign s_ti_and_te = (s_ti & s_te);
+  assign s_ff_d_input = s_d_and_te_n | s_ti_and_te;
 
-  AND_GATE #(
-      .BubblesMask(2'b00)
-  ) GATES_2 (
-      .input1(s_te),
-      .input2(s_ti),
-      .result(s_gates2_out)
-  );
-
-  OR_GATE #(
-      .BubblesMask(2'b00)
-  ) GATES_3 (
-      .input1(s_gates1_out),
-      .input2(s_gates2_out),
-      .result(s_gates3_out)
-  );
-
+  
   D_FLIPFLOP #(
       .InvertClockEnable(0)
   ) MEMORY_4 (
       .clock(s_clk),
-      .d(s_gates3_out),
+      .d(s_ff_d_input),
       .preset(1'b0),
       .q(s_q_out),
       .qBar(s_qn_out),
       .reset(1'b0),
       .tick(1'b1)
   );
+/*
+reg latchD;
 
+assign s_q_out = latchD;
+assign s_qn_out = ~s_q_out;
 
+always @(posedge s_clk, s_ff_d_input) begin
+    if (s_clk) begin
+        latchD <= s_ff_d_input;
+    end
+
+end
+*/
 endmodule
