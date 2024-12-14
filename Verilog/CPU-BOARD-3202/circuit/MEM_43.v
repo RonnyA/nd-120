f3063ad71cb5da757a7c644c07c6ba5253772e30
Ronny Hansen <ronnyha@live.no>
2024-12-14 14:14:59 +0100

    Added signal comments/descriptions

--- Verilog/CPU-BOARD-3202/circuit/MEM_43.v
+++ Verilog/CPU-BOARD-3202/circuit/MEM_43.v
@@ -15,54 +15,54 @@ module MEM_43 (
     input sysclk,    // System clock in FPGA
     input sys_rst_n, // System reset in FPGA
 
-    input        BDAP50_n,
-    input        BDRY50_n,
-    input [ 4:0] BD_23_19_n,
-    input        BIOXE_n,
-    input        BMEM_n,
-    input        DBAPR,
-    input        ECCR,
-    input        ECREQ,
-    input        FETCH,
-    input        GNT_n,
-    input        IBINPUT_n,
-    input        IORQ_n,
-    input [23:0] LBD_23_0_IN,
-    input [23:0] LBD_23_0_OUT,
-    input        MOR_n,
-    input        MR_n,
-    input        OSC,
-    input        PA_n,
-    input        PD1,
-    input        PD3,
-    input        PD4,
-    input [ 4:0] PPN_23_19,
-    input        PS_n,
-    input        REFRQ_n,
-    input        REF_n,
-    input        RERR_n,
-    input        SEMRQ50_n,
-    input        SSEMA_n,
-    input        WRITE,
+    input       BDAP50_n,    //! BUS DAta Present (Delayed 50ns)
+    input       BDRY50_n,    //! BUS Data ReadY (Delayed 50ns)
+    input [4:0] BD_23_19_n,  //! Bus Data and Address (bits 23:19 negated)
+    input       BIOXE_n,     //! BUS IOX Enable
+    input       BMEM_n,      //! BUS MEMORY Enable
+    input       DBAPR,       //! BUS Address Present
+    input       ECCR,        //! BUS ECC Request
+    input       ECREQ,       //! BUS ECC Request
+    input       FETCH,       //! Bus Fetch
+    input       GNT_n,       //! Bus Grant
+    input       IBINPUT_n,   //! Bus Input Enable
+    input       IORQ_n,      //! Bus Input/Output Request
+    input       MOR_n,       //! Memory Error
+    input       MR_n,        //! Master Reset
+    input       OSC,         //! Oscillator
+    input       PA_n,        //! Parity Error Address (PEA)
+    input       PD1,         //! Power Down 1
+    input       PD3,         //! Power Down 3
+    input       PD4,         //! Power Down 4
+    input [4:0] PPN_23_19,   //! Physical Page Number Bits 23:19
+    input       PS_n,        //! Parity Error Signal (PES)
+    input       REFRQ_n,     //! Refresh Request
+    input       REF_n,       //! Refresh
+    input       RERR_n,      //! Refresh Error
+    input       SEMRQ50_n,   //! Semaphore Request
+    input       SSEMA_n,     //! Semaphore
+    input       WRITE,       //! Write
 
+    input [23:0] LBD_23_0_IN,    //! Local Bus Address and Data 23:0 (IN) -  Address and Data for RAM
+    output [23:0] LBD_23_0_OUT,  //! Local Bus Address and Data 23:0 (OUT) - Data from from RAM (15:0)
 
     // Output signals
 
-    output        BDRY_n,
-    output        BGNT50_n,
-    output        BGNT_n,
-    output        CGNT50_n,
-    output        CGNT_n,
-    output        CRQ_n,
-    output        GNT50_n,
-    output [15:0] IDB_15_0_OUT,
-    output        LERR_n,
-    output        LPERR_n,
-    output        MOFF_n,
-    output        MOR25_n,
-    output        MWRITE_n,
-    output        LED4,          // LED4_RED_PARITY_ERROR
-    output        LED_CPU_GI,    // LED_CPU_GRANT_INDICATOR
+    output        BDRY_n,        //! Bus Data Ready
+    output        BGNT50_n,      //! Bus Grant (Delayed 50ns)
+    output        BGNT_n,        //! Bus Grant
+    output        CGNT50_n,      //! CPU Grant (Delayed 50ns)
+    output        CGNT_n,        //! CPU Grant
+    output        CRQ_n,         //! CPU Request
+    output        GNT50_n,       //! CPU Grant (Delayed 50ns)
+    output [15:0] IDB_15_0_OUT,  //! Bus Data 15:0
+    output        LERR_n,        //! Local Error
+    output        LPERR_n,       //! Local Parity Error
+    output        MOFF_n,        //! Memory Off
+    output        MOR25_n,       //! Memory Request (Delayed 25ns)
+    output        MWRITE_n,      //! Memory Write
+    output        LED4,          //! LED4_RED_PARITY_ERROR
+    output        LED_CPU_GI,    //! LED_CPU_GRANT_INDICATOR
     output        LED_BUS_GI     // LED_BUS_GRANT_INDICATOR
 );
 
@@ -76,21 +76,17 @@ module MEM_43 (
   wire [ 9:0] s_aa_9_0;
   wire [15:0] s_idb_15_0_out;
 
-  wire [17:0] s_bus_dd_in;
-  wire [17:0] s_bus_dd_out;
+  wire [17:0] s_data_dd_in;
+  wire [17:0] s_data_dd_out;
 
-  wire [17:0] s_dd_17_0_in;  // INPUT TO MEM_DATA
-  wire [17:0] s_dd_17_0_out; // OUTPUT FROM MEM_DATA
+  wire [17:0] s_ram_dd_17_0_in;  // INPUT TO MEM_DATA
+  wire [17:0] s_ram_dd_17_0_out;  // OUTPUT FROM MEM_DATA
 
   wire [19:0] s_lbd_19_0;
 
-  wire [23:0] s_lbd_23_0_in;  // RAM DATA 16 bits
+  wire [23:0] s_lbd_23_0_in;  // RAM DATA 16 bits and ADDRESS 24 bits
   wire [23:0] s_lbd_23_0_out;  // RAM DATA 16 bits
 
-
-  // TODO: Check that lbd_23_16 (in) and lbd_15_0 (in-out) is behaving as expected
-
-  wire        s_bcgnt25;
   wire        s_bcgnt50;
   wire        s_bcgnt50r_n;
   wire        s_bdap50_n;
@@ -98,7 +94,7 @@ module MEM_43 (
   wire        s_bdry50_n;
   wire        s_bgnt_n;
   wire        s_bgnt25_n;
-  wire        s_bgnt25;
+  wire        s_bcgnt25;
   wire        s_bgnt50_n;
   wire        s_bioxe_n;
   wire        s_bioxl_n;
@@ -203,13 +199,14 @@ module MEM_43 (
   assign MOFF_n              = s_moff_n;
   assign MOR25_n             = s_mor25_n;
   assign MWRITE_n            = s_mwrite_n;
+  assign LBD_23_0_OUT        = s_lbd_23_0_out;
 
 
   /*******************************************************************************
    ** Here all internal busses are connected                                     **
    *******************************************************************************/
-  assign s_bus_dd_in = s_dd_17_0_out;
-  assign s_dd_17_0_in = s_bus_dd_out;
+  assign s_data_dd_in        = s_ram_dd_17_0_out;
+  assign s_ram_dd_17_0_in    = s_data_dd_out;
 
   /*******************************************************************************
    ** Here all sub-circuits are defined                                          **
@@ -285,8 +282,8 @@ module MEM_43 (
   MEM_DATA_46 DATA (
       .BCGNT50R_n(s_bcgnt50r_n),
       .BIOXL_n(s_bioxl_n),
-      .DD_17_0_IN(s_bus_dd_in[17:0]),
-      .DD_17_0_OUT(s_bus_dd_out[17:0]),
+      .DD_17_0_IN(s_data_dd_in[17:0]),
+      .DD_17_0_OUT(s_data_dd_out[17:0]),
       .ECCR(s_eccr),
       .HIEN_n(s_hien_n),
       .HIERR(s_hierr),
@@ -326,7 +323,7 @@ module MEM_43 (
   MEM_RAMC_50 RAMC (
       .BDAP50_n(s_bdap50_n),
       .BDRY50_n(s_bdry50_n),
-      .BGNT25(s_bgnt25),
+      .BCGNT25(s_bcgnt25),
       .BGNT25_n(s_bgnt25_n),
       .BGNT_n(s_bgnt_n),
       .BLRQ50_n(s_blrq50_n),
@@ -352,7 +349,8 @@ module MEM_43 (
 
   MEM_ADDR_44 ADDR (
       .AA_9_0(s_aa_9_0[9:0]),
-      .BCGNT50(s_bcgnt50),
+      //.BCGNT50(s_bcgnt50),
+      .BCGNT50(s_bcgnt25), // Need to latch address earlier before it goes away. Thats why this is changed to BCGNT25 (Ronny 7.12.2024)
       .HIEN_n(s_hien_n),
       .LBD_19_0(s_lbd_23_0_in[19:0]),
       .LOEN_n(s_loen_n),
@@ -368,8 +366,8 @@ module MEM_43 (
       .BANK2(s_bank_2_0[2]),
       .CAS(s_cas),
       .CORR_n(s_corr_n),
-      .DD_17_0_IN(s_dd_17_0_in[17:0]),
-      .DD_17_0_OUT(s_dd_17_0_out[17:0]),  //TODO: Connect
+      .DD_17_0_IN(s_ram_dd_17_0_in[17:0]),
+      .DD_17_0_OUT(s_ram_dd_17_0_out[17:0]),
       .MWRITE50_n(s_mwrite50_n),
       .RAS(s_ras)
   );
