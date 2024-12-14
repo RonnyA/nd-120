9e8e431590f48091a89d9a72b49da0cf5eab36e5
Ronny Hansen <ronnyha@live.no>
2024-12-14 15:43:31 +0100

    Added header

--- Verilog/Shared/support/TTL_74374.v
+++ Verilog/Shared/support/TTL_74374.v
@@ -1,15 +1,23 @@
-// 74LS374
-// Octal Transparent EDGE-TRIGGERED FLIP-FLOPS with 3-state outputs
-// Documentation: https://www.ti.com/lit/ds/symlink/sn54ls373-sp.pdf
+/******************************************************************************
+ **                                                                          **
+ ** Component : 74LS374                                                      **
+ **                                                                          **
+ ** Octal Transparent EDGE-TRIGGERED FLIP-FLOPS with 3-state outputs         ** 
+ **                                                                          **
+ ** Documentation: https://www.ti.com/lit/ds/symlink/sn54ls373-sp.pdf        **
+ **                                                                          **
+ ** Last reviewed: 14-DEC-2024                                               **
+ ** Ronny Hansen                                                             **
+ *****************************************************************************/
 
 module TTL_74374 (
     input wire [7:0] D,   // Data inputs
     input wire CK,        // Clock input (Latching on rising edge of CK)
-    input wire OE_n,      // Output Enable (active low)
+    input wire OE_n,      // Output Control (active low)
     output wire [7:0] Q   // Outputs
 );
     reg [7:0] Q_reg;  // Internal register
-   
+
     // Latch operation on rising edge of CK
     always @(posedge CK) begin
         Q_reg <= D;  // Latch data on rising edge of CK
@@ -19,6 +27,6 @@ module TTL_74374 (
     // When OC_n is low (active), outputs reflect the latched data
     // When OC_n is high, outputs are in high-impedance state
     assign Q = OE_n ? 8'b0 : Q_reg;
-    
+
 
 endmodule
