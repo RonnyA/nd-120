178408396521f25a005337a7d435d291e1b19023
Ronny Hansen <ronnyha@live.no>
2025-01-05 17:43:35 +0100

    Minor refactor + added signal description

--- Verilog/DELILAH-CPU/CGA_MAC/circuit/CGA_MAC_APOS_CALCA.v
+++ Verilog/DELILAH-CPU/CGA_MAC/circuit/CGA_MAC_APOS_CALCA.v
@@ -3,21 +3,29 @@
 ** /CGA/MAC/APOS/CALCA                                                   **
 ** CALCA                                                                 **
 **                                                                       **
+** This module decodes the ECCR IOX register and generates               **
+** the local address (LCA) and memory address (MCA)                      **
+**                                                                       **
+** LCA source is ICA and is latched when MCLK is low                     **
+** MCA source is ICA, and is latched on posedge on MCLK                  **
+**                                                                       **
 ** Page 32                                                               **
 ** SHEET 1 of 1                                                          **
 **                                                                       **
-** Last reviewed: 10-NOV-2024                                            **
+** Last reviewed: 20-DEC-2024                                            **
 ** Ronny Hansen                                                          **
 ***************************************************************************/
 
-module CGA_MAC_APOS_CALCA (
-    input        ECCRHIN,
-    input [15:0] ICA_15_0,
-    input        MCLK,
+// 
 
-    output        ECCR,
-    output [15:0] LCA_15_0,
-    output [ 9:0] MCA_9_0
+module CGA_MAC_APOS_CALCA (
+    input        ECCRHIN,   //! ECCR HI (ECCR Hi bits valid)
+    input [15:0] ICA_15_0,  //! CPU Address Input (16 bits)
+    input        MCLK,      //! Memory Clock
+
+    output        ECCR,      //! DECODE "TRR ECCR" = IOX 100115
+    output [15:0] LCA_15_0,  //! Local Address Output (16 bits)
+    output [ 9:0] MCA_9_0    //! Memory Address Output (10 bits)
 );
 
   /*******************************************************************************
@@ -26,11 +34,10 @@ module CGA_MAC_APOS_CALCA (
   wire [ 9:0] s_mca_9_0_out;
   wire [15:0] s_lca_15_0_out;
   wire [15:0] s_ica_15_0;
-  wire [ 9:0] s_lca_15_0_n_out;
   wire        s_eccr_out;
   wire        s_eccrhi_n;
-  wire        s_gates1_out;
-  wire        s_gates2_out;
+  wire        s_bits_0_4;
+  wire        s_bits_5_9;
   wire        s_mca_10;
   wire        s_mca_11;
   wire        s_mca_12;
@@ -40,6 +47,12 @@ module CGA_MAC_APOS_CALCA (
   wire        s_mclk_n;
   wire        s_mclk;
 
+  wire        s_lca_1_n_out;
+  wire        s_lca_4_n_out;
+  wire        s_lca_5_n_out;
+  wire        s_lca_7_n_out;
+  wire        s_lca_8_n_out;
+  wire        s_lca_9_n_out;
 
   /*******************************************************************************
    ** Here all input connections are defined                                     **
@@ -65,41 +78,45 @@ module CGA_MAC_APOS_CALCA (
   /*******************************************************************************
    ** Here all normal components are defined                                     **
    *******************************************************************************/
+
+  // Decode "TRR ECCR" = IOX 100115
+  // 1000_0000_0100_1101
+
   NAND_GATE_5_INPUTS #(
       .BubblesMask({1'b0, 4'h0})
   ) GATES_1 (
       .input1(s_lca_15_0_out[0]),
-      .input2(s_lca_15_0_n_out[1]),
+      .input2(s_lca_1_n_out),
       .input3(s_lca_15_0_out[2]),
       .input4(s_lca_15_0_out[3]),
-      .input5(s_lca_15_0_n_out[4]),
-      .result(s_gates1_out)
+      .input5(s_lca_4_n_out),
+      .result(s_bits_0_4)
   );
 
   NAND_GATE_5_INPUTS #(
       .BubblesMask({1'b0, 4'h0})
   ) GATES_2 (
-      .input1(s_lca_15_0_n_out[5]),
+      .input1(s_lca_5_n_out),
       .input2(s_lca_15_0_out[6]),
-      .input3(s_lca_15_0_n_out[7]),
-      .input4(s_lca_15_0_n_out[8]),
-      .input5(s_lca_15_0_n_out[9]),
-      .result(s_gates2_out)
+      .input3(s_lca_7_n_out),
+      .input4(s_lca_8_n_out),
+      .input5(s_lca_9_n_out),
+      .result(s_bits_5_9)
   );
+
+  // s_eccrhi_n is generated in CGA_MAC_LA1025
+  // It checks bits 15-10 of the IOX address = 1000_00
+  // Later: Refactor this decoding to be done here (?)
 
   AND_GATE_3_INPUTS #(
       .BubblesMask(3'b111)
   ) GATES_3 (
       .input1(s_eccrhi_n),
-      .input2(s_gates1_out),
-      .input3(s_gates2_out),
+      .input2(s_bits_0_4),
+      .input3(s_bits_5_9),
       .result(s_eccr_out)
   );
 
-
-  /*******************************************************************************
-   ** Here all sub-circuits are defined                                          **
-   *******************************************************************************/
 
   R81 R_LO (
       .CP(s_mclk),
@@ -116,7 +133,7 @@ module CGA_MAC_APOS_CALCA (
       .QAN(),
 
       .QB (s_lca_15_0_out[1]),
-      .QBN(s_lca_15_0_n_out[1]),
+      .QBN(s_lca_1_n_out),
 
       .QC (s_lca_15_0_out[2]),
       .QCN(),
@@ -125,16 +142,16 @@ module CGA_MAC_APOS_CALCA (
       .QDN(),
 
       .QE (s_lca_15_0_out[4]),
-      .QEN(s_lca_15_0_n_out[4]),
+      .QEN(s_lca_4_n_out),
 
       .QF (s_lca_15_0_out[5]),
-      .QFN(s_lca_15_0_n_out[5]),
+      .QFN(s_lca_5_n_out),
 
       .QG (s_lca_15_0_out[6]),
       .QGN(),
 
       .QH (s_lca_15_0_out[7]),
-      .QHN(s_lca_15_0_n_out[7])
+      .QHN(s_lca_7_n_out)
   );
 
   R81 R_HI (
@@ -149,10 +166,10 @@ module CGA_MAC_APOS_CALCA (
       .H (s_mca15),
 
       .QA (s_lca_15_0_out[8]),
-      .QAN(s_lca_15_0_n_out[8]),
+      .QAN(s_lca_8_n_out),
 
       .QB (s_lca_15_0_out[9]),
-      .QBN(s_lca_15_0_n_out[9]),
+      .QBN(s_lca_9_n_out),
 
       .QC (s_lca_15_0_out[10]),
       .QCN(),
