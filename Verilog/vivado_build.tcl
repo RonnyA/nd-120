f618a9b5aa6df9ed9f5e1b5371e1d2681df98647
Ronny Hansen <ronnyhan@gmail.com>
2026-04-10 15:00:37 +0200

    ILA: reduce capture depth from 4096 to 2048 to fit BRAM budget
    Adding 5 new IDB source probes pushed BRAM usage to 106/100. Halving the
    ILA depth halves the BRAM cost while keeping all probes.

--- Verilog/vivado_build.tcl
+++ Verilog/vivado_build.tcl
@@ -167,7 +167,7 @@ create_debug_core u_ila_0 ila
 set_property ALL_PROBE_SAME_MU true [get_debug_cores u_ila_0]
 set_property ALL_PROBE_SAME_MU_CNT 1 [get_debug_cores u_ila_0]
 set_property C_ADV_TRIGGER false [get_debug_cores u_ila_0]
-set_property C_DATA_DEPTH 4096 [get_debug_cores u_ila_0]
+set_property C_DATA_DEPTH 2048 [get_debug_cores u_ila_0]
 set_property C_EN_STRG_QUAL false [get_debug_cores u_ila_0]
 set_property C_INPUT_PIPE_STAGES 0 [get_debug_cores u_ila_0]
 set_property C_TRIGIN_EN false [get_debug_cores u_ila_0]
